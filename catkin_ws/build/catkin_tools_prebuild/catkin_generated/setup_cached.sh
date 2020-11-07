8073b94648cdc1d56e868f4ead535e29e15a74b7
Tommy Clark <tjclark@ualberta.ca>
2020-11-07 10:17:51 -0500

    build catkin workspace

---
+++ catkin_ws/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tommy/AUV-2020/catkin_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export PWD="/home/tommy/AUV-2020/catkin_ws/build/catkin_tools_prebuild"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tommy/AUV-2020/catkin_ws/devel/.private/catkin_tools_prebuild/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tommy/AUV-2020/catkin_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
---
+++ catkin_ws/devel/setup.zsh
@@ -0,0 +1,1 @@
+/home/tommy/AUV-2020/catkin_ws/devel/.private/catkin_tools_prebuild/setup.zsh
