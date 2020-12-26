ac28c55888356abbb264212483a330d46beebdef
Wissam Mantash <mantash.wissam@gmail.com>
2020-12-26 12:18:56 -0500

    solve system of equations to find next state. TODO - stub functions

--- catkin_ws/src/controls/src/thrust_mapper.py
+++ catkin_ws/src/controls/src/thrust_mapper.py
@@ -2,8 +2,13 @@
 
 import rospy
 import numpy as np
+import scipy as sp
 from geometry_msgs.msg import Wrench
 from auv_msgs.msg import ThrusterCommands
+from dimensions.py import D
+
+
+current_state = 0, 0, 0, 0, 0, 0, 0, 0 # order is t1, t2, t3, t4, s1, s2, s3, s4
 
 '''
 - superimposer will publish message (type Wrench) onto topic /controls/wrench
@@ -11,43 +16,51 @@ from auv_msgs.msg import ThrusterCommands
 - thrust_mapper will publish the pwm commands onto /servo_pos topic
 '''
 
-''' Transformation Matrices '''
 
-# TODO - values need to be re-derived
-pwm_to_wrench = matrix([
-        # Fx Fy Fz Mx My Mz
-        [1, 0, 0, 0, 0, 0],             # surge_port
-        [1, 0, 0, 0, 0, 0],             # surge_starboard
-        [0, 1, 0, 0, 0, -0.720],        # sway_bow
-        [0, 1, 0, 0, 0, 0.340],         # sway_stern
-        [0, 0, 1, -0.162, -0.240, 0],   # heave_bow_port
-        [0, 0, 1, 0.162, -0.240, 0],    # heave_bow_starboard
-        [0, 0, 1, -0.162, 0.460, 0],    # heave_stern_port
-        [0, 0, 1, 0.162, 0.460, 0]      # heave_stern_starboard
-    ]).T
+def create_equations(wrench):
+     fx, fy, fz, mx, my, mz = wrench
 
-# TODO - likely the components of the thrust produced at each motor will be determined, 
-# the position of the servo is determined trigonometrically
-wrench_to_servos = np.linalg.pinv(pwm_to_wrench) # TODO - 4x8 matrix defining position of servos
-wrench_to_thrusters = np.linalg.pinv(pwm_to_wrench) # TODO - 4x8 matrix defining action of thrusters
+    def equations(p):
+        t1, t2, t3, t4, s1, s2, s3, s4 = p
+        return (
+                -t2*np.cos(s2) + t4*np.cos(s4) - fx,
+                t1*np.cos(s1) - t3*np.cos(s3) - fy,
+                t1*np.sin(s1) + t2*np.sin(s2) + t3*np.sin(s3) + t4*np.sin(s4) - fz,
+                D*t2*np.sin(s2) - D*t4*np.sin(s4) - mx,
+                -D*t1*np.sin(s1) + D*t3*np.sin(s3) - my,
+                 D*t1*np.cos(s1) + D*t2*np.cos(s2) + D*t3*np.cos(s3) + D*t4*np.cos(s4) - mz
+                 )
+    return equations
 
+def solve_state(wrench):
+
+    return fsolve(create_equations(wrench), current_state) # t1, t2, t3, t4, s1, s2, s3, s4
+
+
+def force_to_pwm(forces):
+    return forces # TODO - map forces to pwm array
+
+
+def angles_to_pwm(angles):
+    return angles #TODO - map angles to pwm array
 
 '''
+# TODO - likely the components of the thrust produced at each motor will be determined,
 Callback when something is published to /controls/wrench topic by superimposer
 maps the wrench onto PWM voltages and publishes them to /servo_pos
 
 @param msg - the published message of type Wrench
 '''
+
 def wrench_callback(data):
 
-    wrench = matrix([data.force.x, data.force.y, data.force.z, 
-                    data.torque.x, data.torque.y, data.torque.z]).T
-    
-    servos = np.matmul(wrench_to_servos, wrench)
-    thrusters = np.matmul(wrench_to_thrusters, wrench)
-
-
-
+    wrench = (data.force.x, data.force.y, data.force.z,
+                    data.torque.x, data.torque.y, data.torque.z)
+    next_state = solve_state(wrench)
+    thrusters = force_to_pwm(next_state[0:4])
+    servos = angles_to_pwm(next_state[4:])
+    pub.publish(thrusters, servos)
+    current_state = next_state
 
 '''
 main point of execution
