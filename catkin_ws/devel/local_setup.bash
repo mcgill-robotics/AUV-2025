4b07610e2cd66a59483bbab2f12fd47a94d42e11
Wissam Mantash <mantash.wissam@gmail.com>
2021-05-15 14:31:58 -0400

    GridSearch helper functions implemented

--- catkin_ws/devel/local_setup.bash
+++ catkin_ws/devel/local_setup.bash
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/local_setup.bash.in
-
-CATKIN_SHELL=bash
-
-# source setup.sh from same directory as this file
-_CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
-. "$_CATKIN_SETUP_DIR/setup.sh" --extend --local
+/home/osboxes/Robotics/AUV-2020/catkin_ws/devel/.private/catkin_tools_prebuild/local_setup.bash
--- catkin_ws/src/planner/src/MockMissionPlanner.py
+++ catkin_ws/src/planner/src/MockMissionPlanner.py
@@ -2,6 +2,7 @@ import rospy
 import smach
 from std_msgs.msg import Bool, Float64
 from blinky.msg import TaskStatus
+from geometry_msgs.msg import Vector3Stamped
 
 # Navigation Target coordinates 
 # X     : DVL
@@ -99,14 +100,100 @@ class GateState(smach.State):
 class GridSearch(smach.State):
     def __init__(self):
         # This Executes a raster scan to search for something, probably a lane
-        # 1) Surge for a predetermined time (long)
-        # 2) Turn 90 degrees using the IMU
-        # 3) S
-        smach.State.__init__(self, outcomes=['pointingToNextTask', 'notSeeingLane'])
+        # 1) Turn 90 degrees using the IMU 
+        # 2) Surge for half the predetermined time (long)
+        # 3) Turn 180 degrees using the IMU
+        # 4) Surge for a predetermined time (long)
+
+        # loop until we find lane or reach max_value
+        # 5) Turn 90 degrees using the IMU
+        # 6) Surge for a predetermined time (short)
+        # 7) Turn 90 degrees using the IMU
+        # 8) Surge for a predetermined time (long), call lane detector after set time
+        # 9) Change sign of 90 degrees turn
+        
+        smach.State.__init__(self, outcomes=['missionSucceeded'])
+        self.SHORT_TIME = 1
+        self.NUMBER_OF_SHORT_PER_LONG = 5 
+        self.MAX_ITERATION = 5
+        self.YAW_THRESHOLD = 0.1*3.14/2
+        self.SURGE_MAGNITUDE = 1
+        self.STABLE_COUNTS = 1
+
+        self.turn_angle = 90
+        self.current_yaw = None 
+        self.current_yaw = 0  
+        self.stable_at_yaw = False   
+        self.done_surging = False   
+        self.current_count = 0
+        self.angle_achieved_time = None 
+
+        self.current_pose_sub = rospy.Subscriber('/robot_state', Vector3Stamped, self.IMU_cb)
+        self.yaw_enable_pub      = rospy.Publisher('/controls/yaw_pid/pid_enable' , Bool    , queue_size=1)
+        self.yaw_setpoint_pub    = rospy.Publisher('/controls/yaw_pid/setpoint'   , Float64 , queue_size=1)
+        self.surge_magnitude_pub   = rospy.Publisher('/controls/superimposer/surge'   , Float64 , queue_size=1)
+
+
+    def IMU_cb(self, msg):
+        self.current_yaw = msg.vector.z
+
+    def turn_90_degrees(self, direction):
+        print("entered turn 90 deg")
+        if(direction == 'left'):
+            print("goign left")
+            target_yaw = self.current_yaw - 3.14/2.0
+        elif(direction=='right'):
+            print("going right")
+            target_yaw = self.current_yaw + 3.14/2.0
+        else:
+            print("Invalid direction. Choose 'left' or 'right'.")
+            return
+
+        self.yaw_setpoint_pub.publish(target_yaw)
+        self.yaw_enable_pub.publish(True)
+        self.current_count   = 0
+        while not self.stable_at_yaw:
+            print("inside 90 deg while loop")
+            print("target_yaw {}".format(target_yaw))
+            if abs(self.current_yaw-target_yaw) < self.YAW_THRESHOLD:
+                print('recieved a stable count')
+                self.current_count += 1
+            else:
+                self.current_count = 0
+
+            if self.current_count > self.STABLE_COUNTS : # We are at correct yaw angle! 
+                print("jolly good")
+                self.stable_at_yaw = True   # I am assuming this never needs to be set false. Danger danger. 
+
+    def move_forward(self):
+         while ( not self.done_surging): # Loop inside here untill we are ready to move to the next state
+            #print out the number of counts we are waiting for
+            self.remaining_counts = self.STABLE_COUNTS - self.current_count
+            if self.remaining_counts > 0 :
+                rospy.loginfo_throttle(1, 'Attaining yaw angle: Need {} more stable readings'.format(self.remaining_counts))      
+
+            if self.stable_at_yaw == True:
+                #Get the timestamp of the first moment the robot is at depth
+                if self.angle_achieved_time is None: 
+                    self.angle_achieved_time = rospy.get_time()
+                # Surge for a known duration, then exit the state
+                self.time_remaining = self.SHORT_TIME - (rospy.get_time() - self.angle_achieved_time)
+                if ( self.time_remaining >0):
+                    self.surge_magnitude_pub.publish(self.SURGE_MAGNITUDE)
+                    rospy.loginfo_throttle(1, 'Surging: Time left is {} seconds'.format(self.time_remaining))
+                else:
+                    self.surge_magnitude_pub.publish(0)
+                    self.done_surging = True
+                    return
+        
 
     def execute(self, userdata):
-        #
-        rospy.loginfo('Executing state LaneDetector')
+        yaw_before_turn = self.current_yaw
+        self.turn_90_degrees('left')
+    
+        self.move_forward()
+        return 'missionSucceeded'
+    
 
         if seeingLane: # !!! this condition is not defined
             return 'pointingToNextTask'
@@ -156,7 +243,7 @@ class BuoyTask(smach.State):
 class NavitageToSurfacingTask(smach.State):
 
     def __init__(self):
-        smach.State.__init__(self, outcomes=['missionSuceeded'])
+        smach.State.__init__(self, outcomes=['missionSucceeded'])
 
         #Defining the subscriber to read the current Hydrophones Heading
         self.hydrophones_sub = rospy.Subscriber('/hydrophones/heading', Float64, self.hydrophones_cb) 
@@ -247,7 +334,7 @@ class NavitageToSurfacingTask(smach.State):
 
 
         if self.successful_surface: # !!! this condition is not defined
-            return 'missionSuceeded'
+            return 'missionSucceeded'
         else:
             pass
 
@@ -256,7 +343,7 @@ def main():
     rospy.init_node('mockMissionPlanner', anonymous=True)
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['missionFailed', 'missionSuceeded'])
+    sm = smach.StateMachine(outcomes=['missionFailed', 'missionSucceeded'])
 
     # Open the container
     with sm:
@@ -280,10 +367,11 @@ def main():
         # smach.StateMachine.add('GateState', GateState(), 
         #                        transitions={'gatePassed':'NavitageToSurfacingTask',
         #                                     'gateMissed':'missionFailed'})
-        
+        smach.StateMachine.add('GridSearch', GridSearch(), 
+                        transitions={'missionSucceeded':'missionSucceeded'})
         # The surface task. Finishes up the mission.
-        smach.StateMachine.add('NavitageToSurfacingTask', NavitageToSurfacingTask(), 
-                        transitions={'missionSuceeded':'missionSuceeded'})
+        #smach.StateMachine.add('NavitageToSurfacingTask', NavitageToSurfacingTask(), 
+        #                transitions={'missionSucceeded':'missionSucceeded'})
 
     # Execute SMACH plan
     outcome = sm.execute()
