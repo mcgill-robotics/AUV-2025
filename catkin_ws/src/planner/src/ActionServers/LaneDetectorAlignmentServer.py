b937367af6478fb71eea37157fe9aa844ececebf
Charles Lapierre <charles.lapierre@bell.net>
2021-07-18 21:55:43 -0400

    Fix problems with datat types in messages for alignment ActionServer
    Alignment ActionServer now works by itself. However, transition between the 2 client methods in MockMissionPlanner does not work. The code seems stuck at client.get_result(). Terminating the ActionServer does not change anything.

--- catkin_ws/src/planner/src/ActionServers/LaneDetectorAlignmentServer.py
+++ catkin_ws/src/planner/src/ActionServers/LaneDetectorAlignmentServer.py
@@ -7,28 +7,35 @@ from planner.msg import LaneDetectorAlignmentAction, LaneDetectorAlignmentFeedba
 from geometry_msgs.msg import Point
 
 class LaneDetectorAlignmentServer():
+
     def __init__(self):
 
-
-        #self.TARGET_ANGLE = 0
+        # self.TARGET_ANGLE = 0 -> variable passed as goal, therefore set in execute_cb()
         self.COUNTS_FOR_STABILITY   = 2 # This should be higher, but we are testing...
         self.current_stable_counts  = 0
-        self.ANGLE_THRESHOLD       = 20*3.14/180 #Chosen for testing 
+        self.ANGLE_THRESHOLD        = 20 * 3.14 / 180 # Chosen for testing (could be passed in goal
+                                                        # along with the target angle using a
+                                                        # custom message? In the future maybe?)
 
-        # Enabling PIDs
-        self.yaw_pid_enable_pub          = rospy.Publisher('/lane_yaw_pid/enable', Bool, queue_size = 1)
-        # Setpoints for PIDs
-        self.yaw_pid_setpoint_pub        = rospy.Publisher('/lane_yaw_pid/setpoint', Float64, queue_size = 1)
-        # Publishing "data" to PIDs
-        self.current_angle_pub = rospy.Publisher('/cv/down_cam_heading_Hough', Float64, queue_size = 1)
+        # self.down_cam_heading_Hough = rospy.Subscriber('/cv/down_cam_heading_Hough',
+            # Float64, self.heading_align_cb) -> subscriber set in execute_cb() because only want it
+            # to start after self.TARGET_ANGLE is set
+
+        # Enabling PID
+        self.yaw_pid_enable_pub     = rospy.Publisher('/lane_yaw_pid/enable', Bool, queue_size = 1)
+        # Setpoints for PID
+        self.yaw_pid_setpoint_pub   = rospy.Publisher('/lane_yaw_pid/setpoint', Float64, queue_size = 1)
+        # No need to publish "data" to PID because it can read straight from CV without any data modification
+        # (PID will read from same topic as the subscriber mentioned above)
 
         # Define the action server, and start it
-        self._action_name = 'LDAlignment'
-        self._as = actionlib.SimpleActionServer(self._action_name, LaneDetectorAlignmentAction,
+        self._action_name   = 'LDAlignment'
+        self._as            = actionlib.SimpleActionServer(self._action_name, LaneDetectorAlignmentAction,
                                                      execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
 
     def execute_cb(self, goal):
+
         rospy.loginfo('Executing Lane Detector Alignment Server')
 
         #Set a variable that will remain true unless the server is pre-empted
@@ -42,12 +49,12 @@ class LaneDetectorAlignmentServer():
         # This is bad practice, but avoids a logical flaw when the subscriber gets to the callback before the VIEWFRAME_CENTER_Y is initialized
         self.down_cam_heading_Hough = rospy.Subscriber('/cv/down_cam_heading_Hough', Float64, self.heading_align_cb)
         
-        # Set the setpoints for the surge and sway PIDs
+        # Set the setpoint
         self.yaw_pid_setpoint_pub.publish(self.TARGET_ANGLE)
-      
-        #Then enable the pids!
+        #Then enable the PID!
         self.yaw_pid_enable_pub.publish(True)
 
+        # Stay stuck in loop until we are aligned and stable (maybe add timeout in future? This could be done from client)
         while not (self.current_stable_counts >= self.COUNTS_FOR_STABILITY):
 
             if self._as.is_preempt_requested():
@@ -61,15 +68,13 @@ class LaneDetectorAlignmentServer():
             self._as.publish_feedback(LaneDetectorAlignmentFeedback(stable_count_angle_remaining = remaining_counts))
 
         if success:
+            
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(result = LaneDetectorAlignmentResult(0.0)) # This is awful and hardcoded, please do what you want with it
 
-    def heading_align_cb(self, angle):
-        #print('Reached callback')
+    def heading_align_cb(self, cvmsg):
 
-        self.current_angle_pub.publish(angle)
-     
-        #print('Centroid distance from center: {}'.format(self.distance_centroid_to_center))
+        angle = cvmsg.data
 
         if (angle < self.ANGLE_THRESHOLD):
                 self.current_stable_counts += 1
