e048dba1abb9f4c9a20b12030b11b77d0f64bda2
Charles Lapierre <charles.lapierre@bell.net>
2021-08-14 14:20:18 -0400

    Write depth action server

--- catkin_ws/src/planner/src/ActionServers/GateDepthServer.py
+++ catkin_ws/src/planner/src/ActionServers/GateDepthServer.py
@@ -11,17 +11,20 @@ class GateDepthServer():
 
         # Constants
         self.COUNTS_FOR_STABILITY   = 2 # This should be higher, but we are testing...
+        self.TARGET_DEPTH           = 0 # meters
+        self.DEPTH_THRESHOLD        = 0 # meters
 
         # Data variables
-        self.stable_counts  = 0
+        self.stable_counts          = 0
+        self.stable_at_depth        = False
 
-        # Publishers to PID
+        # Depth subscriber
+        self.depth_sub              = rospy.Subscriber('/state_estimation/depth', Float64, self.depth_cb)
 
-        # Heave PID
-        self.heave_pid_enable_pub     = rospy.Publisher('/centroid_x_pid/enable', Bool, queue_size = 1)
-        self.heave_pid_setpoint_pub   = rospy.Publisher('/centroid_x_pid/setpoint', Float64, queue_size = 1)
-        self.delta_x_pub             = rospy.Publisher('/lane_detector/centroid_delta_x', Float64, queue_size = 1)
-        # delta_x is "state data" for sway PID
+        # Depth PID publishers
+        self.depth_pid_enable_pub       = rospy.Publisher('/controls/depth_pid/pid_enable' , Bool    , queue_size=1)
+        self.depth_pid_setpoint_pub     = rospy.Publisher('/controls/depth_pid/setpoint'   , Float64 , queue_size=1)
+        # Depth PID gets "data" from depth sensor, no need to publish from here
 
         # Define the action server, and start it
         self._action_name = 'GateDepth'
@@ -30,51 +33,38 @@ class GateDepthServer():
         self._as.start()
 
 
-    def centroid_loc_cb(self, cvmsg):
+    def depth_cb(self, depth_msg):
         
-        point = cvmsg.gravity
-        #print('Reached callback')
-        y_dist_to_center = point.y - self.VIEWFRAME_CENTER_Y
-        x_dist_to_center = point.x - self.VIEWFRAME_CENTER_X
-        self.distance_centroid_to_center = math.sqrt(y_dist_to_center * y_dist_to_center
-                                                    + x_dist_to_center * x_dist_to_center)
+        self.current_depth = depth_msg.data
 
-        self.delta_y_pub.publish(y_dist_to_center)
-        self.delta_x_pub.publish(x_dist_to_center)
-        #print('Centroid distance from center: {}'.format(self.distance_centroid_to_center))
-
-        if (self.distance_centroid_to_center < self.RADIAL_THRESHOLD):
-                self.current_stable_counts += 1
+        if abs(self.current_depth - self.TARGET_DEPTH) < self.DEPTH_THRESHOLD :
+            self.stable_counts += 1
         else:
-                self.current_stable_counts = 0
+            self.stable_counts = 0
 
         return
 
 
     def execute_cb(self, goal):
-        rospy.loginfo('Executing Lane Detector Centering Server')
+        rospy.loginfo('Achieveing depth')
 
-        #Set a variable that will remain true unless the server is pre-empted
+        # Set a variable that will remain true unless the server is pre-empted
         success = True
         
         print(goal)
 
         # Unpack the goal message into its components
-        self.VIEWFRAME_CENTER_Y = goal.image_center_point.y
-        self.VIEWFRAME_CENTER_X = goal.image_center_point.x
-        self.RADIAL_THRESHOLD = 0.3 * self.VIEWFRAME_CENTER_Y * 2
-
-        # This is bad practice, but avoids a logical flaw when the subscriber gets to the callback before the VIEWFRAME_CENTER_Y is initialized
-        self.centroid_sub = rospy.Subscriber('cv/down_cam_target_centroid', CvTarget, self.centroid_loc_cb)
+        self.TARGET_DEPTH = goal.depth_target.data
+        self.DEPTH_THRESHOLD = 0.1 * self.TARGET_DEPTH
 
         # Set the setpoints for the surge and sway PIDs
-        self.surge_pid_setpoint_pub.publish(0)
-        self.sway_pid_setpoint_pub.publish(0)
-        #Then enable the pids!
-        self.surge_pid_enable_pub.publish(True)
-        self.sway_pid_enable_pub.publish(True)
+        self.depth_pid_setpoint_pub.publish(self.TARGET_DEPTH)
+        # Then enable the pids!
+        self.depth_pid_enable_pub.publish(True)
+        # Reset stable counts
+        self.stable_counts = 0
 
-        while not (self.current_stable_counts >= self.COUNTS_FOR_STABILITY):
+        while not (self.stable_counts >= self.COUNTS_FOR_STABILITY):
 
             if self._as.is_preempt_requested():
                 rospy.loginfo('%s: Preempted' % self._action_name)
@@ -82,18 +72,17 @@ class GateDepthServer():
                 success = False
                 break
 
-            remaining_counts = self.COUNTS_FOR_STABILITY - self.current_stable_counts
-            rospy.loginfo_throttle(1, 'Moving towards centroid: Need {} more stable readings'.format(remaining_counts))
-            self._as.publish_feedback(GateDepthFeedback(stable_count_centroid_remaining = remaining_counts))
+            remaining_counts = self.COUNTS_FOR_STABILITY - self.stable_counts
+            rospy.loginfo_throttle(1, 'Going to depth: Need {} more stable readings'.format(remaining_counts))
+            self._as.publish_feedback(DepthFeedback(current_depth = Float64(self.current_depth)))
 
         if success:
             rospy.loginfo('%s: Succeeded' % self._action_name)
-            self._as.set_succeeded(result = GateDepthResult(Point(x=0, y=0))) # This is awful and hardcoded, please do what you want with it
+            self._as.set_succeeded(result = GateResult(current_depth = Float64(self.current_depth)))
 
         # We should turn off PIDs no matter if we succeed or not
-        print("Turning off surge and sway PIDs")
-        self.surge_pid_enable_pub.publish(False)
-        self.sway_pid_enable_pub.publish(False)
+        print("Turning off heave PID")
+        self.depth_pid_enable_pub.publish(False)
 
         return
 
