fed1800d6e86305b76e64c71199cffde2b47017e
Charles Lapierre <charles.lapierre@bell.net>
2021-08-18 20:22:33 -0400

    Actionize surging towards gate.
    Enough flexibility was left to potentially add image detection for the gate

--- catkin_ws/src/planner/src/Tasks/S1_GateTask.py
+++ catkin_ws/src/planner/src/Tasks/S1_GateTask.py
@@ -5,7 +5,7 @@ import time
 
 from blinky.msg import TaskStatus # Blinky commented out for now TODO
 from std_msgs.msg import Bool, Float64
-from planner.msg import DepthAction, DepthGoal, DepthResult
+from planner.msg import DepthAction, DepthGoal, DepthResult, GatePassThroughAction, GatePassThroughGoal, GatePassThroughResult
 
 class GateTask(smach.State):
     # The gate state : ded reckoning
@@ -97,27 +97,21 @@ class GateTask(smach.State):
 
         print("Successfully reached depth")
 
-        return 'gatePassed'# TODO Still need to implement the surging ActionServer
+        # GatePassThrough Action
+        print("Starting GatePassThrough client")
+        print("Creating client...")
+        client = actionlib.SimpleActionClient('GatePassThrough_Action', GatePassThroughAction)
+        print("Client created, waiting for server...")
+        client.wait_for_server()
+        print("Found server, creating goal...")
+        goal = GatePassThroughGoal(search_for_gate = Bool(False), surge_magnitude = Float64(self.SURGE_MAGNITUDE), surge_time = Float64(self.SURGE_DURATION))
+        print("Goal created, sending goal...")
+        print(goal)
+        client.send_goal(goal)
+        print("Goal sent, waiting for result...")
+        client.wait_for_result()
+        print("Result received")
+        print(client.get_result())
+        print("Stop centering ActionServer")
 
-        # rospy.loginfo('Inside Gate State')
-
-        # # Check if we are stable at the setpoint? 
-        # while (not self.done_surging): # Loop inside here untill we are ready to move to the next state
-        #     #print out the number of counts we are waiting for
-        #     self.remaining_counts = self.COUNTS_FOR_STABILITY - self.stable_counts
-        #     if self.remaining_counts > 0 :
-        #         rospy.loginfo_throttle(1, 'Attaining Depth: Need {} more stable readings'.format(self.remaining_counts))      
-
-        #     if self.stable_at_depth == True:
-        #         #Get the timestamp of the first moment the robot is at depth
-        #         if self.depth_achieved_time is None: 
-        #             self.depth_achieved_time = rospy.get_time()
-        #         # Surge for a known duration, then exit the state
-        #         self.time_remaining = self.SURGE_DURATION - (rospy.get_time() - self.depth_achieved_time)
-        #         if (self.time_remaining > 0):
-        #             self.surge_magnitude_pub.publish(self.SURGE_MAGNITUDE)
-        #             rospy.loginfo_throttle(1, 'Surging: Time left is {} seconds'.format(self.time_remaining))
-        #         else:
-        #             self.surge_magnitude_pub.publish(0)
-        #             self.done_surging = True
-        #             return 'gatePassed'
+        return 'gatePassed'
