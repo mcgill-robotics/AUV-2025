79aa20e772ecb387b5f634d48141d54c2120f137
Charles Lapierre <charles.lapierre@bell.net>
2021-08-28 12:37:50 -0400

    Remove unnecessary code in GateTask and keep depth PID on if depth achieved in depth ActionServer

--- catkin_ws/src/planner/src/Tasks/S1_GateTask.py
+++ catkin_ws/src/planner/src/Tasks/S1_GateTask.py
@@ -31,49 +31,12 @@ class GateTask(smach.State):
         self.SURGE_MAGNITUDE       = 1   # one day I'll know what these units are. Start small and increment in pool testing.
         self.SURGE_DURATION        = 10  # seconds
 
-        # Variables
-        self.stable_counts         = 0   # counter variable to determine if we are stable at the setpoint
-        self.stable_at_depth       = False
-        self.depth_achieved_time   = None 
-        self.done_surging          = False
-
-        # Depth subscriber
-        self.depth_sub            = rospy.Subscriber('/state_estimation/depth', Float64, self.depth_cb)
-
-        # Depth PID publishers
-        self.depth_enable_pub     = rospy.Publisher('/controls/depth_pid/pid_enable' , Bool    , queue_size=1)
-        self.depth_setpoint_pub   = rospy.Publisher('/controls/depth_pid/setpoint'   , Float64 , queue_size=1)
-        # Depth PID gets "data" from depth sensor, no need to publish from here
-
-        # Surge publisher
-        self.surge_magnitude_pub  = rospy.Publisher('/controls/superimposer/surge'   , Float64 , queue_size=1)
-
         # Define the publisher for blinky, publish a task...just a random one for now
         #self.blinky_pub            = rospy.Publisher('/task'   , TaskStatus , queue_size=1)
         #self.taskmsg        = TaskStatus()
         #self.taskmsg.action = 1
         #self.taskmsg.task   = 1
         #self.blinky_pub.publish (taskmsg)
-        
-        # Turn on depth PID
-        self.depth_setpoint_pub.publish(self.DEPTH_SETPOINT)
-        self.depth_enable_pub.publish(True) 
-
-
-    def depth_cb(self, msg):
-
-        self.depth = msg.data # takes the depth float 64 from the subscriber and sets it to a variable
-        print('inside callback. Current depth: {} '.format(self.depth))
-
-        # Check for stability at depth
-        if abs(self.depth - self.DEPTH_SETPOINT) < self.DEPTH_THRESHOLD :
-            #print('recieved a stable count')
-            self.stable_counts += 1
-        else:
-            self.stable_counts   = 0
-
-        if self.stable_counts >= self.COUNTS_FOR_STABILITY : # We are at depth! 
-            self.stable_at_depth = True   # I am assuming this never needs to be set false. Danger danger. TODO
 
 
     def execute(self, userdata):
