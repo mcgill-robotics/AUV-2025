c822d572edd65f8120ff58b59c8583239969853a
Rafid Saif <rsaif22@hotmail.com>
2021-09-04 13:08:42 -0400

    Removed unnecessary imports in Controls

--- catkin_ws/src/controls/src/controls/__init__.py
+++ catkin_ws/src/controls/src/controls/__init__.py
@@ -5,25 +5,6 @@
 # Controllers
 from controllers import *
 
-# Maintainers
-from maintainers import *
-
-# Utils
-from utils import *
-
 __author__ = 'Justin Bell, Jana Pavlasek, Jeremy Mallette'
 
-__all__ = ['AcousticServoController',
-           'SonarServoController',
-           'FrontVisualServoController',
-           'DownVisualServoController',
-           'DepthMaintainer',
-           'YawMaintainer',
-           'RollMaintainer',
-           'PID',
-           'trans_gains',
-           'rot_gains',
-           'SyncServoController',
-           'AsyncServoController',
-           'normalize_angle',
-           'transform_polygon']
+__all__ = []
