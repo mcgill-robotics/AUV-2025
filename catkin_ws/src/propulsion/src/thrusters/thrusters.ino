aa441b27600132ccc17555196455edd8cfb6e1f9
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-01-23 20:56:42 +0000

    implement thrusters.ino, make ThrusterCommands.msg scoped within propulsion pkg

--- catkin_ws/src/propulsion/src/thrusters/thrusters.ino
+++ catkin_ws/src/propulsion/src/thrusters/thrusters.ino
@@ -1,150 +1,63 @@
-#include "thrusters.h"
+#include <ros.h>
+#include <Servo.h>
+#include <propulsion/ThrusterCommand.h>
 
-int thrusterBoundCheck(int input) {
-  if (abs(input) > THRUSTER_OUTPUT_BOUND) {
-    nh.logerror("Thrusters command out of bound!");
-    return max(-THRUSTER_OUTPUT_BOUND, min(THRUSTER_OUTPUT_BOUND, input));
-  }
-  return input;
+#define SRG_P_PIN 	4
+#define SRG_S_PIN	5
+#define SWY_BW_PIN 	6
+#define SWY_ST_PIN 	7
+#define HVE_BW_P_PIN 	8
+#define HVE_BW_S_PIN 	9
+#define HVE_ST_S_PIN 	10
+#define HVE_ST_P_PIN 	11
+
+/* less verbose identifiers */
+const uint8_t SRG_P 	= propulsion::ThrusterCommand::SURGE_PORT
+const uint8_t SRG_S 	= propulsion::ThrusterCommand::SURGE_STAR
+const uint8_t SWY_BW 	= propulsion::ThrusterCommand::SWAY_BOW
+const uint8_t SWY_ST 	= propulsion::ThrusterCommand::SWAY_STERN
+const uint8_t HVE_BW_P 	= propulsion::ThrusterCommand::HEAVE_BOW_PORT
+const uint8_t HVE_BW_S 	= propulsion::ThrusterCommand::HEAVE_BOW_STAR
+const uint8_t HVE_ST_S 	= propulsion::ThrusterCommand::HEAVE_STERN_STAR
+const uint8_t HVE_ST_P 	= propulsion::ThrusterCommand::HEAVE_STERN_PORT
+
+Servo thrusters[] = new Servo[8];
+
+ros::NodeHandle nh;
+ros::Subscriber<propulsion::ThrusterCommand> 
+	sub("propulsion/thruster_cmd", &commandCallback);
+
+
+void commandCb(propulsion::ThrusterCommand command){
+	thrusters[SRG_P].writeMicroSeconds(command[SRG_P_PIN]);
+	thrusters[SRG_S].writeMicroSeconds(command[SRG_S_PIN]);
+	thrusters[SWY_BW].writeMicroSeconds(command[SWY_BW_PIN]);
+	thrusters[SWY_ST].writeMicroSeconds(command[SWY_ST_PIN]);
+	thrusters[HVE_BW_P].writeMicroSeconds(command[HVE_BW_P_PIN]);
+	thrusters[HVE_BW_S].writeMicroSeconds(command[HVE_BW_S_PIN]);
+	thrusters[HVE_ST_P].writeMicroSeconds(command[HVE_ST_P_PIN]);
+	thrusters[HVE_ST_S].writeMicroSeconds(command[HVE_ST_S_PIN]);
 }
 
-int thrusterSlopeCheck(int last_input, int new_input) {
-  int output = max(last_input - THRUSTER_MAX_INCREMENT, new_input);
-  output = min(last_input + THRUSTER_MAX_INCREMENT, new_input);
-  return output;
-}
 
-void thrustersCallback(const auv_msgs::ThrusterCommands& msg) {
-  if (mission_enabled) {
-    thrusters_reset_schedule = millis() + THRUSTER_TIMEOUT;
-
-    for (uint8_t i = 0; i < THRUSTER_COUNT; i++) {
-      int16_t value = thrusterBoundCheck(msg.thruster_commands[i]);
-      value = thrusterSlopeCheck(last_thruster_commands[i], value);
-      thrusters[i].writeMicroseconds(THRUSTER_RST_VALUE + value);
-
-      last_thruster_commands[i] = value;
-    }
-  } else {
-    nh.logwarn("Mission off, thrusters command IGNORED!!");
-  }
-}
-
-void vacuumCallback(const std_msgs::Int16& msg) {
-  if (mission_enabled) {
-    vacuum_reset_schedule = millis() + THRUSTER_TIMEOUT;
-
-    int16_t value = thrusterBoundCheck(msg.data);
-    value = thrusterSlopeCheck(last_vacuum_command, value);
-    vacuum.writeMicroseconds(THRUSTER_RST_VALUE + value);
-    last_vacuum_command = value;
-  } else {
-    nh.logwarn("Mission off, vacuum command IGNORED!!");
-  }
-}
-
-void resetThrusters() {
-  for (uint8_t i = 0; i < THRUSTER_COUNT; i++) {
-    last_thruster_commands[i] = 0;
-    thrusters[i].writeMicroseconds(THRUSTER_RST_VALUE);
-  }
-  nh.loginfo("Thrusters reset!!");
-}
-
-void thrustersInit() {
-  for (uint8_t i = 0; i < THRUSTER_COUNT; i++) {
-    thrusters[i].attach(thruster_pins[i]);
-  }
-  resetThrusters();
-}
-
-void resetVacuum() {
-  last_vacuum_command = 0;
-  vacuum.writeMicroseconds(THRUSTER_RST_VALUE);
-  nh.loginfo("Vacuum reset!!");
-}
-
-void vacuumInit() {
-  vacuum.attach(vacuum_pin);
-  resetVacuum();
-}
-
-void gpioInit() {
-//  pinMode(VOLTAGE_PIN, INPUT);
-  pinMode(MISSION_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
-}
-
-void rosInit() {
-  nh.initNode();
-
-  nh.advertise(voltage_pub);
-  nh.advertise(mission_pub);
-  nh.subscribe(vacuum_sub);
-  nh.subscribe(thrusters_sub);
+void initThrusters(){
+	thrusters[SRG_P].attach(SRG_P_PIN)
+	thrusters[SRG_S].attach(SRG_S_PIN)
+	thrusters[SWY_BW].attach(SWY_BW_PIN)
+	thrusters[SWY_ST].attach(SWY_ST_PIN)
+	thrusters[HVE_BW_P].attach(HVE_BW_P_PIN)
+	thrusters[HVE_BW_S].attach(HVE_BW_S_PIN)
+	thrusters[HVE_ST_S].attach(HVE_ST_S_PIN)
+	thrusters[HVE_ST_P].attach(HVE_ST_P_PIN)
+	//TODO - set initial thruster effort (OFF)
 }
 
 void setup() {
-  thrustersInit();
-  vacuumInit();
-  gpioInit();
-  rosInit();
-  if (MCUSR & MCUSR_WDRF) {
-    MCUSR ^= MCUSR_WDRF;
-    nh.logerror("Watchdog reset occurred!");
-    nh.spinOnce();
-  }
-  wdt_enable(WDTO_1S);
+	initThrusters();
+	nh.initNode();
 }
 
-void voltageReportTask(unsigned long time_now) {
-  if (voltage_report_schedule < time_now) {
-    voltage_m.data = analogRead(VOLTAGE_PIN) * VOLT_SLOPE + VOLT_OFFSET;
-    voltage_pub.publish(&voltage_m);
-    voltage_report_schedule += VOLTAGE_REPORT_INTERVAL;
-    toggleLed();
-  }
-}
-
-void thurstersResetTask(unsigned long time_now) {
-  if (thrusters_reset_schedule < time_now) {
-    if (mission_enabled) {
-      nh.logwarn("Thrusters command timeout!");
-    }
-    resetThrusters();
-    thrusters_reset_schedule = time_now + TRRUSTER_RESET_INTERVAL;
-    toggleLed();
-  }
-}
-
-void vacuumResetTask(unsigned long time_now) {
-  if (vacuum_reset_schedule < time_now) {
-    if (mission_enabled) {
-      nh.logwarn("Vacuum command timeout!");
-    }
-    resetVacuum();
-    vacuum_reset_schedule = time_now + TRRUSTER_RESET_INTERVAL;
-    toggleLed();
-  }
-}
-
-void missionReportTask(unsigned long time_now) {
-  if (misssion_report_schedule < time_now) {
-    mission_m.data = mission_enabled;
-    mission_pub.publish(&mission_m);
-    misssion_report_schedule += MISSION_REPORT_INTERVAL;
-    toggleLed();
-  }
-}
 void loop() {
-  wdt_reset();
-  unsigned long time_now = millis();
-  mission_enabled = digitalRead(MISSION_PIN);
-
-  thurstersResetTask(time_now);
-  vacuumResetTask(time_now);
-  missionReportTask(time_now);
-  voltageReportTask(time_now);
-
-  nh.spinOnce();
+	// listen for commands
+	nh.spinOnce(); 
 }
