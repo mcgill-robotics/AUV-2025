3d233e93fc57c20e3f07e9f901f1751fa0ecaf40
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-01-30 02:50:28 +0000

    create controls package - buildable/runnable

---
+++ catkin_ws/src/controls/src/pursueTarget.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import actionlib
+import rospy
+
+from std_msgs.msg import Float64
+from auv_msgs.msg import PursueTargetAction
+
+class pursue_target_server:
+    epsilon = 0.05 # permissible error [m]
+
+    def __init__(self):
+        self.pub = rospy.Publisher('pid_setpoint', Float64, queue_size=50)
+        self.server = actionlib.SimpleActionServer('pursueTarget', PursueTargetAction, self.execute_cb, False)
+        self.server.start()
+
+    def execute_cb(self, goal):
+
+        def state_cb(self, state):
+            self.server.publish_feedback(state.depth)
+            if self.goal.depth - state.depth < epsilon:
+                self.server.set_succeeded()
+                
+        # compute effort using PID
+        self.pub.publish(goal.depth)
+        self.sub = rospy.Subscriber('state', Float64, state_cb, queue_size=50)
+
+
+if __name__ == '__main__':
+    rospy.init_node('pursueTargetServer')
+    server = pursue_target_server()
+    rospy.spin()
+
