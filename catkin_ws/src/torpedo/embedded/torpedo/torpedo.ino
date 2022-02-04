837aba2ecf9fe04a9060af751e3b8a40b59732f7
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-02-04 02:28:09 +0000

    package acts as a switch to toggle releasing torpedo (via electronics) - buildable

--- catkin_ws/src/torpedo/embedded/torpedo/torpedo.ino
+++ catkin_ws/src/torpedo/embedded/torpedo/torpedo.ino
@@ -1,20 +1,23 @@
-#include <Arduino.h>
-#define ENABLE 3
-#define THRUSTER 9
-#define PWM_EN 255
-#define PWM_OFF 0
+#include <ros.h>
+#include <std_msgs/Bool.h>
+
+#define ENABLE_SWITCH 3
 
 void setup(){
-	pinMode(ENABLE, INPUT);
-	pinMode(THRUSTER, OUTPUT);
-	digitalWrite(THRUSTER, LOW);
+	pinMode(ENABLE_SWITCH, INPUT);
+	digitalWrite(ENABLE_SWITCH, LOW);
 }
 
+void commandCb(const std_msgs::Bool& enable){
+	if(enable.data){
+		digitalWrite(ENABLE_SWITCH, HIGH);
+	}
+}
+
+ros::NodeHandle nh;
+ros::Subscriber<std_msgs::Bool> sub("torpedo_enable", &commandCb);
+
 void loop(){
-	if (digitalRead(ENABLE)){
-		analogWrite(THRUSTER, PWM_EN);
-	}
-	else{
-		analogWrite(THRUSTER, PWM_OFF);
-	}
+	// listen for commands
+	nh.spinOnce(); 
 }
