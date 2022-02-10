db8cf8166bb4784855ee01b794982d78dea620d5
Olivier Hepler <olivierhepler@gmail.com>
2022-02-10 10:54:08 -0800

    Depth.py file changes

--- catkin_ws/src/state_estimation/depth.py
+++ catkin_ws/src/state_estimation/depth.py
@@ -5,8 +5,10 @@ from std_msgs.msg import Float64
 
 
 if __name__ == '__main__':
-	rospy.init_node('depth')
-	pub = rospy.Publisher('state', Float64, queue_size=50)
-	data = Float64(3.14)
-	pub.publish(data)
-	rospy.spin()
+    print('helloworld')
+    rospy.init_node('depth')
+    pub = rospy.Publisher('state', Float64, queue_size=50)
+    data = Float64(3.14)
+    while True:
+        pub.publish(data)
+    rospy.spin()
