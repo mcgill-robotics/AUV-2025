56764cf85941f6b0e6b983ec414283c56a37e084
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-02-12 20:24:34 +0000

    planner - runnable submerge goal - TODO: handle returned action object

--- catkin_ws/src/planner/src/mission.py
+++ catkin_ws/src/planner/src/mission.py
@@ -4,13 +4,15 @@ import rospy
 import smach
 
 from submerge import SubmergingState
+from std_msgs.msg import Float64
 
 if __name__ == '__main__':
     rospy.init_node('mission_planner')
-    sm = smach.StateMachine(outcomes=['success', 'failure'])
+    sm = smach.StateMachine(outcomes=['success', 'failure']) 
     with sm:
         smach.StateMachine.add('submerging', SubmergingState(), 
                 transitions={'submerging_success': 'success', 'submerging_failure':'failure'})
         print(sm.get_active_states())
-    res = sm.execute(10)
+
+    res = sm.execute()
     print("mission complete, result: ", res)
