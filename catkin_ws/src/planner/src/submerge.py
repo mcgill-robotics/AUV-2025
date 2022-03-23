cb7ef433e3bbcbad886f5fbb4ff4daf7ffbf1fb3
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-03-23 05:22:03 +0000

    temp - fix launch setpoint bug

--- catkin_ws/src/planner/src/submerge.py
+++ catkin_ws/src/planner/src/submerge.py
@@ -10,15 +10,17 @@ class SubmergingState(smach.State):
     def __init__(self):
         super().__init__(outcomes=['submerging_success', 'submerging_failure'])
         self.client = actionlib.SimpleActionClient('pursueTarget', PursueTargetAction)
-        print('waiting for server')
-        self.client.wait_for_server()
-        print('state detected')
 
     def execute(self, ud):
+        print('waiting for server')
+        self.client.wait_for_server()
+        print('server detected')
+
         target_depth = Float64(4.0)
         print("submerging to", target_depth)
         goal = PursueTargetGoal(target_depth)
         self.client.send_goal(goal)
+        print("goal sent")
         res = self.client.wait_for_result()
         print("finished submerging, result:", res)
 
