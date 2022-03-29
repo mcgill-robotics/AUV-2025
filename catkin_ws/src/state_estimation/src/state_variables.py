45e452b6f1cae827e8888f8e76d3b598709fe9d2
vasily <vasilyfedotov12@gmail.com>
2022-03-29 01:39:38 -0400

    state_estimation launcheable

--- catkin_ws/src/state_estimation/src/state_variables.py
+++ catkin_ws/src/state_estimation/src/state_variables.py
@@ -12,28 +12,28 @@ class _State_Var:
 
 class X(_State_Var):
     def __init__(self):
-        self.super().__init__()
+        super().__init__()
 
 class Y(_State_Var):
     def __init__(self):
-        self.super().__init__()
+        super().__init__()
 
 class Z(_State_Var):
     def __init__(self):
-        self.super().__init__()
-        rospy.subscriber('depth', Float64, self.depth_sensor_cb)
+        super().__init__()
+        rospy.Subscriber('depth', Float64, self.depth_sensor_cb)
 
     def depth_sensor_cb(self, depth):
         self.val = depth 
 
 class Theta_X(_State_Var):
     def __init__(self):
-        self.super().__init__()
+        super().__init__()
 
 class Theta_Y(_State_Var):
     def __init__(self):
-        self.super().__init__()
+        super().__init__()
 
 class Theta_Z(_State_Var):
     def __init__(self):
-        self.super().__init__()
+        super().__init__()
