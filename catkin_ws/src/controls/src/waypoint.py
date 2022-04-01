18e16b8a7ea13b3e2d1b3e464828feddd0e88225
Vasily Fedotov <vasilyfedotov12@gmail.com>
2022-04-01 05:58:54 +0000

    change pursueTarget -> waypoint, remove unnecessary files

--- catkin_ws/src/controls/src/waypoint.py
+++ catkin_ws/src/controls/src/waypoint.py
@@ -9,7 +9,40 @@ from state import Setpoint_State, Perceived_State
 from std_msgs.msg import Float64, Bool
 from tf import quaternion_multiply
 
+# set up PID for rotation
+# - the 'position variable' being controlled with the pid
+# is the angle (theta) through which the robot is rotated around the 
+# axis of rotation specified by sin(theta/2)(xi + yj + zk)
+# where the orientation is described by quaternion <w, x, y, z>
+# where w = cos(theta/2)
+#
+# - keep track of axis of rotation, during its rotation, should
+# the robot deviate from rotating about this axis a new axis
+# of rotation should be calculated that corresponds to a rotation
+# that brings the robot into the target orientation. From this
+# axis a quaternion can be constucted that describes the intended
+# transformation. This would include updating the theta setpoint
+# for the PID according to the new value of w.
+#
+# let the current orientation be described by q_1 (relative to datum)
+# thus p_curr = q_1 * p_datum * q_1_inv => p_datum = q_1_inv * p_curr * q_1
+#
+# let the target orientation be described by q_2 (relative to datum)
+# thus p_target = q_2 * p_datum * q_2_inv
+# 
+# a new quaternion decribing the transformation from the current orientation
+# to the target orientation is found by:
+# p_target = q_2 * (q_1_inv * p_curr * q_1) * q_2_inv
+#
+# thus q_3 = q_2 * q_1_inv
+# 
+#           q_1 = w_1 + x_1*i + y_1*j + z_1*k 
+#               = cos(theta/2) + sin(theta/2)[x_1*i + y_1*j + z_1*k]
+#   =>  q_1_inv = cos(-theta/2) + sin(-theta/2)[x_1*i + y_1*j + z_1*k]
+#               = w_1 - x_1*i - y_1*j - z_1*k 
+
 # values to reuse
+
 enable = Bool(True)
 disable = Bool(False)
 
