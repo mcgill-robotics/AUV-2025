fix controls runntime issues with quaternion impl

catkin_ws/src/controls/src/state.py
@@ -7,8 +7,8 @@ from tf.transformations import quaternion_multiply
 
 class AUV_State:
     def __init__(self, pose_msg):
-        self.position = Position(pose_msg.position)
-        self.orientation = Orientation(pose_msg.orientation)
+        self.position = Position.from_msg(pose_msg.position)
+        self.orientation = Orientation.from_msg(pose_msg.orientation)
 
     def as_pose(self):
         pos = Point(x=self.position.x, 
@@ -27,10 +27,11 @@ class Setpoint_State(AUV_State):
     def __init__(self, pose_msg):
         super().__init__(pose_msg)
 
-        self.x_pub = Publisher("x_setpoint", Float64, queue_size=50)
-        self.y_pub = Publisher("y_setpoint", Float64, queue_size=50)
-        self.z_pub = Publisher("z_setpoint", Float64, queue_size=50)
-        self.dtheta_pub = Publisher("dtheta_setpoint", Float64, queue_size=50)
+        self.x_pub = rospy.Publisher("x_setpoint", Float64, queue_size=50)
+        self.y_pub = rospy.Publisher("y_setpoint", Float64, queue_size=50)
+        self.z_pub = rospy.Publisher("z_setpoint", Float64, queue_size=50)
+        self.theta_pub = rospy.Publisher("theta_setpoint", Float64, queue_size=50)
+        self.dtheta_pub = rospy.Publisher("dtheta_setpoint", Float64, queue_size=50)
 
     def update(self, pose_msg):
         self.position.update(pose_msg.position)
@@ -41,14 +42,13 @@ class Setpoint_State(AUV_State):
         self.x_pub.publish(Float64(self.position.x))
         self.y_pub.publish(Float64(self.position.y))
         self.z_pub.publish(Float64(self.position.z))
-        # do not publish theta - it requires knowledge of curr_state
-        # since what actually gets published is the difference
-        # in angle between target/current state
+        self.theta_pub.publish(Float64(self.orientation.theta))
+
 
     def publish_angle_diff(self, curr_state):
         # dtheta is angle between this and the current state
-        q3 = self.orientation.transform_to(target_state.orientation)
-        self.dtheta_pub.publish(Float64(q3.orientation.theta))
+        q3 = curr_state.orientation.transform_to(self.orientation)
+        self.dtheta_pub.publish(Float64(q3.theta))
 
 
 
@@ -56,16 +56,16 @@ class Setpoint_State(AUV_State):
 To be used for setting PID state
 '''
 class Perceived_State(AUV_State):
-    def __init__(self):
+    def __init__(self, pose_msg):
         super().__init__(pose_msg)
 
-        self.x_pub = Publisher("x_state", Float64, queue_size=50)
-        self.y_pub = Publisher("y_state", Float64, queue_size=50)
-        self.z_pub = Publisher("z_state", Float64, queue_size=50)
-        self.theta_pub = Publisher("theta_state", Float64, queue_size=50)
+        self.x_pub = rospy.Publisher("x_state", Float64, queue_size=50)
+        self.y_pub = rospy.Publisher("y_state", Float64, queue_size=50)
+        self.z_pub = rospy.Publisher("z_state", Float64, queue_size=50)
+        self.theta_pub = rospy.Publisher("theta_state", Float64, queue_size=50)
 
 
-    def publish(self, target_state):
+    def publish(self):
         self.x_pub.publish(Float64(self.position.x))
         self.y_pub.publish(Float64(self.position.y))
         self.z_pub.publish(Float64(self.position.z))
@@ -77,10 +77,18 @@ encapsulates a point describing robot's
 location relative to a datum (global) axis
 '''
 class Position:
-    def __init__(self, pos_msg):
-        self.x = pos_msg.x
-        self.y = pos_msg.y
-        self.z = pos_msg.z
+    
+    def from_msg(pos_msg):
+        x = pos_msg.x
+        y = pos_msg.y
+        z = pos_msg.z
+        return Position(x, y, z)
+
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
 
 '''
@@ -88,19 +96,21 @@ encapsulates a quaternion describing robot's
 rotation relative to a datum (global) axis
 '''
 class Orientation:
-    def __init__(self, orient_msg):
-        self.x = orient_msg.x
-        self.y = orient_msg.y
-        self.z = orient_msg.z
-        self.w = orient_msg.w
-        self.theta = acos(self.w*2)
     
+    def from_msg(orient_msg):
+        x = orient_msg.x
+        y = orient_msg.y
+        z = orient_msg.z
+        w = orient_msg.w
+        return Orientation(x, y, z, w)
+
     def __init__(self, x, y, z, w):
         self.x = x
         self.y = y
         self.z = z
         self.w = w
-        self.theta = acos(self.w*2)
+        self.theta = acos(self.w)*2
+
 
     def inverse(self):
         return Orientation(-self.x, -self.y, -self.z, self.w)
@@ -125,5 +135,5 @@ class Orientation:
         return q2.mult(self.inverse())
 
     def axis_of_rotation(self):
-        a = asin(self.theta*2)
+        a = asin(self.theta)*2
         return [a*self.x, a*self.y, a*self.z]