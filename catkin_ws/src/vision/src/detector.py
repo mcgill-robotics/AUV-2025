f407316c309d0b6f634f6e5a397164267351672e
Olivier Hepler <olivierhepler@gmail.com>
2022-06-09 18:07:19 -0800

    vision package work

--- catkin_ws/src/vision/src/detector.py
+++ catkin_ws/src/vision/src/detector.py
@@ -3,32 +3,34 @@ import rospy
 import numpy as np
 
 from sensor_msgs.msg import Image
-#from std_msgs.msg import Boolean
+from std_msgs.msg import Bool
 
 
 
 def image_cb(image_msg):
 
-  width = image_msg.width
-  height = image_msg.height
+    width = image_msg.width
+    height = image_msg.height
+    
+    print(image_msg.encoding)
 
-  img_data = list(image_msg.data) 
+    print("step value: ",image_msg.step,", width val:",image_msg.width,", height val:",image_msg.height,", data points:",len(image_msg.data))
 
-
-  img_matrix = np.array(img_data)
-  img_matrix = np.reshape(img_matrix, (width, height, -1))
-
-  
-  pixel_blue = img_matrix[:,:,0]
-
-          
-  print(np.all(pixel_blue[pixel_blue >  200]))
-
+    img_data = list(image_msg.data)
+        
+    #encoding = bgr, 0=b 1=g 2=r value in range below   
+    for i in range(2,len(image_msg.data),3):
+        if img_data[i] > 150:
+            print("red")
 
 
 if __name__ == '__main__':
     print("detector")
+    
     rospy.init_node('detector')
-    #image_sub = rospy.Subscriber('/image_publisher_1651798201951158778/image_raw', Image, image_cb, queue_size=50)
-    #pub = rospy.Publisher('object_detected', Boolean, queue_size=50)
+    
+    image_sub = rospy.Subscriber('/image_publisher/image_raw', Image, image_cb, queue_size=50) 
+    
+    pub = rospy.Publisher('object_detected', Bool, queue_size=50)
+    
     rospy.spin()
