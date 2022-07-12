158f6fee9e6b17c8b3e4a79dea586e58f0a3a647
vasily <vasilyfedotov12@gmail.com>
2022-07-12 14:32:45 -0400

    pool test - test direct motion for each dof

--- catkin_ws/src/planner/src/test_yaw.py
+++ catkin_ws/src/planner/src/test_yaw.py
@@ -2,35 +2,33 @@
 
 import rospy
 import smach
-from pooltest import Pause
+
+from pooltest import DeadReckonMotion, Pause
 from std_msgs.msg import Float64
 
+class Yaw(DeadReckonMotion):
+    def __init__(self, effort, duration=2.0):
+        super().__init__('yaw', effort, duration)
 
-class Yaw(smach.State):
+class Heave(DeadReckonMotion):
+    def __init__(self, effort, duration=2.0):
+        super().__init__('heave', effort, duration)
 
-    def __init__(self, effort, duration):
-        super().__init__(outcomes=['done'])
-        self.effort = Float64(effort)
-        self.duration = duration
-        self.pub_yaw = rospy.Publisher('yaw', Float64, queue_size=50)
 
-    def execute(self, userdata):
-        rospy.Timer(rospy.Duration(0.1), self.update)
-        rospy.sleep(self.duration)
-        return 'done'
-
-    def update(self, effort):
-        self.pub_yaw.publish(self.effort)
-
-def main():
-        rospy.init_node('smach_yaw_tester')
-        sm = smach.StateMachine(outcomes=['finished'])
-        with sm:
-            smach.StateMachine.add('ClockWise', Yaw(effort=-.5, duration=3.0), transitions={'done':'pause'})
-            smach.StateMachine.add('pause', Pause(duration=1.5), transitions={'done':'CounterClockWise'})
-            smach.StateMachine.add('CounterClockWise', Yaw(effort=.8, duration=3.0), transitions={'done':'off'})
-            smach.StateMachine.add('off', Pause(), transitions={'done':'finished'})
-        outcome = sm.execute()
-    
 if __name__ == '__main__':
-    main()
+    rospy.init_node('pool_testing_yaw')
+    sm =smach.StateMachine(outcomes=['finished'])
+
+    with sm:
+        smach.StateMachine.add('submerge', Heave(effort=-0.20, duration = 3.0),
+            transitions={'done':'pause'})
+        smach.StateMachine.add('pause', Pause(duration=2.0), 
+            transitions={'done':'positive'})
+        smach.StateMachine.add('positive', Yaw(effort=0.20), 
+            transitions={'done':'negative'})
+        smach.StateMachine.add('negative', Yaw(effort=-0.20), 
+            transitions={'done':'off'})
+        smach.StateMachine.add('off', Pause(), 
+            transitions={'done':'finished'})
+
+    res = sm.execute()
