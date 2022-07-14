6487381a3bb760f0c7e7afd0fc422d13ea104208
vasily <vasilyfedotov12@gmail.com>
2022-07-14 01:02:03 -0400

    thruster commands test

---
+++ catkin_ws/src/planner/src/pooltest/01-thruster-commands/test_heave.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import rospy
+import smach
+
+from thruster_states import Heave, Pause
+
+if __name__ == '__main__':
+    rospy.init_node('pool_testing_heave')
+    sm =smach.StateMachine(outcomes=['finished'])
+
+    with sm:
+        smach.StateMachine.add('positive', Heave(effort=0.2), 
+            transitions={'done':'pause'})
+        smach.StateMachine.add('pause', Pause(duration=2.0), 
+            transitions={'done':'negative'})
+        smach.StateMachine.add('negative', Heave(effort=-0.2), 
+            transitions={'done':'off'})
+        smach.StateMachine.add('off', Pause(), 
+            transitions={'done':'finished'})
+
+    res = sm.execute()
