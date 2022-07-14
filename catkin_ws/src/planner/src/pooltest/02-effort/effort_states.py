12decb367927365a838d55224d30fbea7f971155
vasily <vasilyfedotov12@gmail.com>
2022-07-13 23:59:14 -0400

    use Wrench testing effort - TODO: launchable separate from other test groups

--- catkin_ws/src/planner/src/pooltest/02-effort/effort_states.py
+++ catkin_ws/src/planner/src/pooltest/02-effort/effort_states.py
@@ -3,14 +3,14 @@
 import rospy
 import smach
 
-from geometry_msgs import Twist, Force, Torque
+from geometry_msgs.msg import Wrench, Vector3, Vector3
 
 class EffortMotion(smach.State):
-    def __init__(self, twist, duration=2.0):
+    def __init__(self, wrench, duration=2.0):
         super().__init__(outcomes=['done'])
-        self.effort = twist
+        self.effort = wrench
         self.duration = duration
-        self.pub = rospy.Publisher('effort', Twist, queue_size=50)
+        self.pub = rospy.Publisher('effort', Wrench, queue_size=50)
     
     def execute(self, ud):
         timer = rospy.Timer(rospy.Duration(0.1), self.update)
@@ -26,56 +26,56 @@ class EffortMotion(smach.State):
 
 class Surge(EffortMotion):
     def __init__(self, effort=10.0, duration=10.0):
-        force = Force(x=effort, y=0.0, z=0.0)
-        torque = Torque(x=0.0, y=0.0, z=0.0)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=effort, y=0.0, z=0.0)
+        torque = Vector3(x=0.0, y=0.0, z=0.0)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
 
 
 class Sway(EffortMotion):
     def __init__(self, effort=10.0, duration=10.0):
-        force = Force(x=0.0, y=effort, z=0.0)
-        torque = Torque(x=0.0, y=0.0, z=0.0)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=0.0, y=effort, z=0.0)
+        torque = Vector3(x=0.0, y=0.0, z=0.0)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
 
 
 class Heave(EffortMotion):
     def __init__(self, effort=10.0, duration=10.0):
-        force = Force(x=0.0, y=0.0, z=effort)
-        torque = Torque(x=0.0, y=0.0, z=0.0)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=0.0, y=0.0, z=effort)
+        torque = Vector3(x=0.0, y=0.0, z=0.0)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
 
 
 class Roll(EffortMotion):
     def __init__(self, effort=10.0, duration=10.0):
-        force = Force(x=0.0, y=0.0, z=0.0)
-        torque = Torque(x=effort, y=0.0, z=0.0)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=0.0, y=0.0, z=0.0)
+        torque = Vector3(x=effort, y=0.0, z=0.0)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
 
 
 class Pitch(EffortMotion):
     def __init__(self, effort=10.0, duration=10.0):
-        force = Force(x=0.0, y=0.0, z=0.0)
-        torque = Torque(x=0.0, y=effort, z=0.0)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=0.0, y=0.0, z=0.0)
+        torque = Vector3(x=0.0, y=effort, z=0.0)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
 
 
 class Yaw(EffortMotion):
     def __init__(self, effort=10.0, duration=10.0):
-        force = Force(x=0.0, y=0.0, z=0.0)
-        torque = Torque(x=0.0, y=0.0, z=effort)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=0.0, y=0.0, z=0.0)
+        torque = Vector3(x=0.0, y=0.0, z=effort)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
 
 
 class Pause(EffortMotion):
 
     def __init__(self, duration=2.0):
-        force = Force(x=0.0, y=0.0, z=0.0)
-        torque = Torque(x=0.0, y=0.0, z=0.0)
-        twist = Twist(force=force, torque=torque)
-        super().__init__(twist, duration)
+        force = Vector3(x=0.0, y=0.0, z=0.0)
+        torque = Vector3(x=0.0, y=0.0, z=0.0)
+        wrench = Wrench(force=force, torque=torque)
+        super().__init__(wrench, duration)
