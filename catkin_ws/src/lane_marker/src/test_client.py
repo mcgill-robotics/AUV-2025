2ec684b4528244aa77aed081a8783e46a1009588
rsaif22.com <rsaif22@hotmail.com>
2022-07-22 13:34:47 -0400

    Incorporated using ros service

---
+++ catkin_ws/src/lane_marker/src/test_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rospy
+from cv_bridge import CvBridge
+from lane_marker.srv import LanesInfo, LanesInfoRequest
+from sensor_msgs.msg import Image
+import cv2
+
+image = cv2.imread('source.png')
+rospy.init_node('image_service_client',anonymous=True)
+bridge = CvBridge()
+ros_img = bridge.cv2_to_imgmsg(image,'bgr8')
+rospy.wait_for_service('lanes_info')
+
+try:
+    proxy = rospy.ServiceProxy('lanes_info',LanesInfo)
+    resp = proxy(ros_img)
+    print('Center at:\n',resp.path_marker_center)
+    print('Heading from last task:\n',resp.path_marker_heading_from)
+    print('Heading to next task:\n',resp.path_marker_heading_to)
+    imgResult = bridge.imgmsg_to_cv2(resp.path_marker_result_img)
+    cv2.imshow('Lanes',image)
+    cv2.imshow('Result',imgResult)
+    cv2.waitKey(5000)
+    cv2.destroyAllWindows()
+except rospy.ServiceException as e:
+    print(str(e))
