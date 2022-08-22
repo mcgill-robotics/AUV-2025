6235d098d31d8ae3b4da6354db317acd0ce94ed2
rsaif22 <rsaif22@hotmail.com>
2022-08-22 11:03:01 -0400

    Initial firmware without header and rosserial implementation

--- catkin_ws/src/depth_sensor/embedded/depth_sensor/depth_sensor.ino
+++ catkin_ws/src/depth_sensor/embedded/depth_sensor/depth_sensor.ino
@@ -1,21 +1,40 @@
 #include <ros.h>
+#include <Wire.h>
+#include <SparkFun_MS5803_I2C.h>
 #include <std_msgs/Float64.h>
 
-#define DEPTH_PIN 5 
+//#define USE_USBCON
 #define DELAY 1000 
+#define RHO 1000
+#define G_VALUE 9.81
+
+MS5803 pressureSensor(ADDRESS_LOW);
+float basePressure, currentPressure;
 ros::NodeHandle nh;
 
 std_msgs::Float64 depthmsg;
 ros::Publisher depth("depth", &depthmsg);
 
+void sensorInit() {
+  Wire.begin();
+  pressureSensor.reset();
+}
+
+float getDepth() {
+  currentPressure = pressureSensor.getPressure(ADC_4096);
+  return((currentPressure-basePressure) / (G_VALUE*RHO));
+}
+
 void setup() {
-  pinMode(DEPTH_PIN, INPUT);
+  sensorInit();
+  basePressure = pressureSensor.getPressure(ADC_4096);
+  nh.getHardware()->setBaud(57600);
   nh.initNode();
   nh.advertise(depth);  
 }
 
 void loop() {
-  depthmsg.data = analogRead(DEPTH_PIN);
+  depthmsg.data = getDepth();
   depth.publish(&depthmsg);
   nh.spinOnce();
   delay(DELAY);
