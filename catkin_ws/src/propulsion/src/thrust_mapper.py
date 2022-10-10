01e0392982f475105bf92ab1a26f62513fedf339
vasily <vasilyfedotov12@gmail.com>
2022-10-10 15:49:32 -0400

    use wrench_to_thrust as subscriber callback

--- catkin_ws/src/propulsion/src/thrust_mapper.py
+++ catkin_ws/src/propulsion/src/thrust_mapper.py
@@ -24,31 +24,27 @@ T = np.matrix(
 
 T_inv = np.linalg.pinv(T) # matrix transformation wrench -> thrust  
 
+def wrench_to_thrust(w):
+    '''
+    wrench_to_thrust maps a Wrench into an intensity [-1.0, 1.0]
+    for each thruster
+    '''
+    a = np.array(
+            [[w.force.x],
+            [w.force.y],
+            [w.force.z],
+            [w.torque.x],
+            [w.torque.y],
+            [w.torque.z]]
+            )
 
-def effort_cb():
-    
-    def wrench_to_thrust(w):
-        '''
-        wrench_to_thrust returns a function, configured with thrust
-        characterisations, that maps a Wrench into thrust intensity (TODO)
-        [-1.0, 1.0]
-        '''
-        a = np.array(
-                [[w.force.x],
-                [w.force.y],
-                [w.force.z],
-                [w.torque.x],
-                [w.torque.y],
-                [w.torque.z]]
-                )
+    b = np.matmul(T_inv, a) 
+    tc = ThrusterCommand([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]]) 
+    thrust_pub.publish(tc)
+    return
 
-        b = np.matmul(T_inv, a) 
-        tc = ThrusterCommand([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]]) 
-        thrust_pub.publish(tc)
-
-    return wrench_to_thrust
 
 if __name__ == '__main__':
     rospy.init_node('thrust_mapper')
-    sub = rospy.Subscriber('/effort', Wrench, effort_cb())
+    sub = rospy.Subscriber('/effort', Wrench, wrench_to_thrust)
     rospy.spin()
