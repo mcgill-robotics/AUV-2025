1a8cd9d7a2547df633e5266b5d6b6154a51c5b6d
Ayoub Sabri <ayoubsabri1410@gmail.com>
2022-10-20 16:31:05 -0400

    Change update_effort function

--- catkin_ws/src/controls/src/superimposer.py
+++ catkin_ws/src/controls/src/superimposer.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
 import rospy
-import numpy as np
+import numpy as n
 
-from geometry_msgs.msg import Vector3, Wrench 
-from std_msgs.msg import Float64 
+from geometry_msgs.msg import Vector3, Wrench, WrenchStamped 
+from std_msgs.msg import Float64, Header
 
 class Superimposer:
     def __init__(self):
         # force
+        self.heave = Superimposer.Degree_Of_Freedom('heave')
         self.surge = Superimposer.Degree_Of_Freedom('surge')
         self.sway = Superimposer.Degree_Of_Freedom('sway')
-        self.heave = Superimposer.Degree_Of_Freedom('heave')
 
         # torque
-        self.roll = Superimposer.Degree_Of_Freedom('roll')
-        self.pitch = Superimposer.Degree_Of_Freedom('pitch')
-        self.yaw = Superimposer.Degree_Of_Freedom('yaw')
+	self.roll = Superimposer.Degree_Of_Freedom('roll')
+	self.pitch = Superimposer.Degree_Of_Freedom('pitch')
+	self.yaw = Superimposer.Degree_Of_Freedom('yaw')
 
         self.pub = rospy.Publisher('effort', Wrench, queue_size=50)
 
 
     def update_effort(self, _):
-        # force and torque are vectors in the robot ref. frame
-        force = Vector3(self.surge.val, self.sway.val, self.heave.val)
-        torque = Vector3(self.roll.val, self.pitch.val, self.yaw.val)
-        wrench = Wrench(force=force, torque=torque) 
+	
+	force = Vector3(self.surge.val, self.sway.val, self.heave.val)
+	torque = Vector3(self.roll.val, self.pitch.val, self.yaw.val)
+	wrench =  Wrench(force=force, torque=torque)
 
-        self.pub.publish(wrench)
+	self.pub.publish(wrench)
 
 
     class Degree_Of_Freedom:
@@ -37,6 +37,7 @@ class Superimposer:
         
         def set_cb(self, new_val):
             self.val = new_val.data
+
 
 
 if __name__ == '__main__':
