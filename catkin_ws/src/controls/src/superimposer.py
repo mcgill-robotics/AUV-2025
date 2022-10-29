41f84dfe379dbfad4a95c88557a5e5d2ad9daa56
YvonaAci <yvona.acimovic@mail.mcgill.ca>
2022-10-29 19:12:31 -0400

    tutorial-1-exercise-yvona

--- catkin_ws/src/controls/src/superimposer.py
+++ catkin_ws/src/controls/src/superimposer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
-import numpy as np
 
-from geometry_msgs.msg import Vector3, Wrench, WrenchStamped 
-from std_msgs.msg import Float64, Header
-from tf2_ros import Buffer, TransformListener 
+from geometry_msgs.msg import Vector3, Wrench 
+from std_msgs.msg import Float64
 
 class Superimposer:
     def __init__(self):
@@ -19,35 +17,16 @@ class Superimposer:
         self.pitch = Superimposer.Degree_Of_Freedom('pitch')
         self.yaw = Superimposer.Degree_Of_Freedom('yaw')
 
-        # tf2 buffer
-        self.tf_buffer = Buffer()
-        TransformListener(self.tf_buffer) 
         self.pub = rospy.Publisher('effort', Wrench, queue_size=50)
-
-        # avoid creating a new Header object for every update
-        # just update the time
-        self.header = Header(frame_id="world")
 
 
     def update_effort(self, _):
-        self.header.stamp = rospy.Time(0)
-
-        # force and torque are vectors in the world ref. frame
-        # they need to be converted into the ref. frame of the robot
-        # before being published as wrench
-        force_world = Vector3(self.surge.val, self.sway.val, self.heave.val)
-        torque_world = Vector3(self.roll.val, self.pitch.val, self.yaw.val)
-        wrench_world = Wrench(force=force_world, torque=torque_world) 
+        # assume force and torque are vectors in the robot ref. frame
+        force = Vector3(self.surge.val, self.sway.val, self.heave.val)
+        torque = Vector3(self.roll.val, self.pitch.val, self.yaw.val)
+        wrench = Wrench(force=force, torque=torque) 
   
-        # transform is computed on stamped message
-        wrench_world_stmp = WrenchStamped(header=self.header, wrench=wrench_world)
-
-        try:
-            # force/torque vectors in robot's ref. frame
-            wrench_auv = self.tf_buffer.transform(wrench_world_stmp, "auv_base")
-            self.pub.publish(wrench_auv.wrench)
-        except Exception as e:
-            print(type(e), e)
+        self.pub.publish(wrench)
 
 
     class Degree_Of_Freedom:
