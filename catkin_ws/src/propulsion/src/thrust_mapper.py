1ba088486ffc615797114b27ee14fcd6f9dd33fa
antoinedang <antoine.dangeard@gmail.com>
2022-10-29 20:08:19 -0400

    Implemented force_to_intensity and pid controller
    Made force to intensity such that the motor intensity is scaled logarithmically to the input force (i.e. on lower forces it will be slightly more powerful, between higher forces it will change rpm less).
    
    I've implemented the PID controller in controls and believe the implementation is correct.
    
    However, both give errors when building propulsion or when building controls, i get missing package errors for the packages rosserial_arduino and PID respectively.

--- catkin_ws/src/propulsion/src/thrust_mapper.py
+++ catkin_ws/src/propulsion/src/thrust_mapper.py
@@ -3,14 +3,14 @@
 import numpy as np
 import rospy
 
-from auv_msgs.msg import ThrusterCommand
+from auv_msgs.msg import ThrusterForces
 from geometry_msgs.msg import Wrench
 
 d = 0.224 # m
 D_1 = 0.895 # m
 D_2 = 0.778 # m
 
-thrust_pub = rospy.Publisher('propulsion/thruster_cmd', ThrusterCommand, queue_size=5)
+thrust_pub = rospy.Publisher('propulsion/thruster_forces', ThrusterForces, queue_size=5)
 rospy.sleep(7.0) #TODO: FIX - wait for 7 sec to sync with arduino?
 
 T = np.matrix(
@@ -24,31 +24,27 @@ T = np.matrix(
 
 T_inv = np.linalg.pinv(T) # matrix transformation wrench -> thrust  
 
+def wrench_to_thrust(w):
+    '''
+    wrench_to_thrust maps a Wrench into an intensity [-1.0, 1.0]
+    for each thruster
+    '''
+    a = np.array(
+            [[w.force.x],
+            [w.force.y],
+            [w.force.z],
+            [w.torque.x],
+            [w.torque.y],
+            [w.torque.z]]
+            )
 
-def effort_cb():
-    
-    def wrench_to_thrust(w):
-        '''
-        wrench_to_thrust returns a function, configured with thrust
-        characterisations, that maps a Wrench into thrust intensity (TODO)
-        [-1.0, 1.0]
-        '''
-        a = np.array(
-                [[w.force.x],
-                [w.force.y],
-                [w.force.z],
-                [w.torque.x],
-                [w.torque.y],
-                [w.torque.z]]
-                )
+    b = np.matmul(T_inv, a) 
+    tc = ThrusterForces([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]]) 
+    thrust_pub.publish(tc)
+    return
 
-        b = np.matmul(T_inv, a) 
-        tc = ThrusterCommand([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]]) 
-        thrust_pub.publish(tc)
-
-    return wrench_to_thrust
 
 if __name__ == '__main__':
     rospy.init_node('thrust_mapper')
-    sub = rospy.Subscriber('/effort', Wrench, effort_cb())
+    sub = rospy.Subscriber('/effort', Wrench, wrench_to_thrust)
     rospy.spin()
