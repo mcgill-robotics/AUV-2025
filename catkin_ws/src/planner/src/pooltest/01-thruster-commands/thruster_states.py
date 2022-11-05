291085168076137c716023132b2837e12021ac63
ben-jamming <benhepditch@gmail.com>
2022-11-05 22:23:37 +0100

    change test-01 to use thruster_intensities

--- catkin_ws/src/planner/src/pooltest/01-thruster-commands/thruster_states.py
+++ catkin_ws/src/planner/src/pooltest/01-thruster-commands/thruster_states.py
@@ -3,14 +3,14 @@
 import rospy
 import smach
 
-from auv_msgs.msg import ThrusterCommand 
+from auv_msgs.msg import ThrusterIntensities
 
 class ThrusterMotion(smach.State):
     def __init__(self, cmd, duration=2.0):
         super().__init__(outcomes=['done'])
         self.effort = cmd
         self.duration = duration
-        self.pub = rospy.Publisher('propulsion/thruster_cmd', ThrusterCommand, queue_size=50)
+        self.pub = rospy.Publisher('propulsion/thruster_intensities', ThrusterIntensities, queue_size=50)
     
     def execute(self, ud):
         timer = rospy.Timer(rospy.Duration(0.1), self.update)
@@ -26,42 +26,42 @@ class ThrusterMotion(smach.State):
 
 class Surge(ThrusterMotion):
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([effort, effort, 0, 0, 0, 0, 0, 0])
+        cmd = ThrusterIntensities([effort, effort, 0, 0, 0, 0, 0, 0])
         super().__init__(cmd, duration)
 
 
 class Sway(ThrusterMotion):
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([0, 0, effort, effort, 0, 0, 0, 0])
+        cmd = ThrusterIntensities([0, 0, effort, effort, 0, 0, 0, 0])
         super().__init__(cmd, duration)
 
 
 class Heave(ThrusterMotion):
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([0, 0, 0, 0, effort, effort, effort, effort])
+        cmd = ThrusterIntensities([0, 0, 0, 0, effort, effort, effort, effort])
         super().__init__(cmd, duration)
 
 
 class Roll(ThrusterMotion):
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([0, 0, 0, 0, -effort, effort, effort, -effort])
+        cmd = ThrusterIntensities([0, 0, 0, 0, -effort, effort, effort, -effort])
         super().__init__(cmd, duration)
 
 
 class Pitch(ThrusterMotion):
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([0, 0, 0, 0, effort, effort, -effort, -effort])
+        cmd = ThrusterIntensities([0, 0, 0, 0, effort, effort, -effort, -effort])
         super().__init__(cmd, duration)
 
 
 class Yaw(ThrusterMotion):
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([-effort, effort, effort, -effort, 0, 0, 0, 0])
+        cmd = ThrusterIntensities([-effort, effort, effort, -effort, 0, 0, 0, 0])
         super().__init__(cmd, duration)
 
 
 class Pause(ThrusterMotion):
 
     def __init__(self, effort=0.2, duration=10.0):
-        cmd = ThrusterCommand([0, 0, 0, 0, 0, 0, 0, 0])
+        cmd = ThrusterIntensities([0, 0, 0, 0, 0, 0, 0, 0])
         super().__init__(cmd, duration)
