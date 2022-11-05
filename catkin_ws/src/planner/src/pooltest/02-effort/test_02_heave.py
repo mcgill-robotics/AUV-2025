a4762f264cbbeb5cbfe1ccd91357153bca317112
ben-jamming <benhepditch@gmail.com>
2022-11-05 19:53:15 +0100

    added pause state in front of pooltestheave.py

--- catkin_ws/src/planner/src/pooltest/02-effort/test_02_heave.py
+++ catkin_ws/src/planner/src/pooltest/02-effort/test_02_heave.py
@@ -10,11 +10,13 @@ if __name__ == '__main__':
     sm =smach.StateMachine(outcomes=['finished'])
 
     with sm:
-        smach.StateMachine.add('positive', Heave(effort=15), 
+        smach.StateMachine.add('startup', Pause(duration=2.0), 
+            transitions={'done':'positive'})
+        smach.StateMachine.add('positive', Heave(effort=50), 
             transitions={'done':'pause'})
         smach.StateMachine.add('pause', Pause(duration=2.0), 
             transitions={'done':'negative'})
-        smach.StateMachine.add('negative', Heave(effort=-15), 
+        smach.StateMachine.add('negative', Heave(effort=-50), 
             transitions={'done':'off'})
         smach.StateMachine.add('off', Pause(), 
             transitions={'done':'finished'})
