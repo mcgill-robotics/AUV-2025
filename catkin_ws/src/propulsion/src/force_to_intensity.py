74835b596215c06b542aa6e317bf04d6a23ab51f
antoinedang <antoine.dangeard@gmail.com>
2022-11-19 19:22:13 -0500

    forgot to commit bug fixes on VM
    small changes for variable naming, correct import. Forgot to commit as change was on VM so redoing pull request

--- catkin_ws/src/propulsion/src/force_to_intensity.py
+++ catkin_ws/src/propulsion/src/force_to_intensity.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
 import rospy
+import math
 from auv_msgs.msg import ThrusterForces, ThrusterIntensities
 
 
 # forces produced by T200 thruster at 16V (N)
 MAX_FWD_FORCE = 5.25*9.81 
 MAX_BKWD_FORCE = 4.1*9.81
+stiffness = 0.0 #optional parameter, must be >= 0, increase to make motor movement stiffer/more reactive at low speeds
 
 def force_to_intensity(force):
-        motor_thrust = 0
         # cap our input force at maximum fwd/bkwd speeds
-        force = min(max(force, -MAX_FORCE_BKWD), MAX_FORCE_FWD)
+        force = min(max(force, -MAX_BKWD_FORCE), MAX_FWD_FORCE)
         if force > 0:
-            max_log = math.log2(MAX_FORCE_FWD) #get log value of maximum fwd force
+            max_log = math.log2(MAX_FWD_FORCE) #get log value of maximum fwd force
             return (math.log2(force)+2+stiffness)/(max_log+2+stiffness) #+2 to make the logs start at 0
         elif force < 0:
-            max_log = math.log2(MAX_FORCE_BKWD) #get log value of maximum bkwd force
+            max_log = math.log2(MAX_BKWD_FORCE) #get log value of maximum bkwd force
             return -(math.log2(-force)+2+stiffness)/(max_log+2+stiffness) #we negate the final return value since we want a negative intensity backwards
         else: return 0
 
