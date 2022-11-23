d886a3709ae9fcf44477c91529469078fc74c52b
Chani Mores <chani.mores@gmail.com>
2022-11-23 14:48:18 -0500

    Add heave command to submerge AUV before calibrating rotation

--- catkin_ws/src/calibration/src/heave-calibration.py
+++ catkin_ws/src/calibration/src/heave-calibration.py
@@ -4,13 +4,15 @@ import rospy
 
 from std_msgs.msg import Float64
 
-pub = rospy.Publisher('/theta_y_setpoint', Float64, queue_size=5)
+pub_theta_y = rospy.Publisher('/theta_y_setpoint', Float64, queue_size=5, latch=True)
+pub_z = rospy.Publisher('/z_setpoint', Float64, queue_size=5, latch=True)
 
 if __name__ == '__main__':
 	rospy.init_node('heave_calibration')
 	
-	rospy.sleep(5)
+	# Move AUV down to submerge itself
+	pub_z.publish(-0.5)
 	
-	# Want to publish theta of y = 0 radians (level)
-	pub.publish(0.0)
+	# Want the AUV to level itself (such that theta_y = 0)
+	pub_theta_y.publish(0.0)
 	rospy.spin()
