6700414f3fc2d55ab7dd20ca3cff5e40e2e7537c
vasily <vasilyfedotov12@gmail.com>
2023-01-15 17:13:08 -0500

    change ROS_MASTER_URI to hardcoded value

--- set_env.bash
+++ set_env.bash
@@ -3,7 +3,5 @@
 # set ROS_IP to the IP adress of this host machine
 export ROS_IP=$(ip r | grep ^192 | awk '{print $9}')
 
-if [ -z "${ROS_MASTER_URI}" ];
-
-    then export ROS_MASTER_URI=192.168.0.105:$1
-fi
+# master node is run on Jetson
+export ROS_MASTER_URI=http://192.168.0.105:11311
