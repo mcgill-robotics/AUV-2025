fce9baec2bd6c5372331d2e837e6c2057cde5887
ben-jamming <benhepditch@gmail.com>
2023-01-20 01:31:08 +0100

    Fixed set_env.bash ROS_IP empty string bug

--- set_env.bash
+++ set_env.bash
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # set ROS_IP to the IP adress of this host machine
-export ROS_IP=$(ip r | grep ^192 | awk '{print $9}')
+IP=$(ip r | grep ^192 | awk '{print $9}');
+if [ -z '$IP' ]; then
+    export ROS_IP=$IP;
+fi
+
 
 # master node is run on Jetson
 export ROS_MASTER_URI=http://192.168.0.105:11311
