2e74f18bf349b585fbd7a166c14f9e23a7927a3e
ben-jamming <benhepditch@gmail.com>
2023-02-26 21:48:02 +0100

    fixed record camera node so it doesn't crash or run out of memory

--- catkin_ws/src/vision/src/record_cameras.py
+++ catkin_ws/src/vision/src/record_cameras.py
@@ -18,9 +18,11 @@ def image_cb(raw_img, camera_name):
     global imgs
     global lastFrameTime
     #if time from when last image was received is greater than videoSaveTime we start a new video
-    if time.time() - lastFrameTime.get(camera_name, time.time()+10) > videoSaveTime: save_images_to_video(shutdown=False)
+    if time.time() - lastFrameTime.get(camera_name, time.time()+10) > videoSaveTime:
+    	save_images_to_video(shutdown=False)
     #save video in smaller increments (to avoid losing everything in case of crash)
-    if len(imgs.get(camera_name, [])) > maxFramesPerVideo: save_images_to_video(shutdown=False)
+    if len(imgs.get(camera_name, [])) > maxFramesPerVideo[camera_name]:
+    	save_images_to_video(shutdown=False)
     lastFrameTime[camera_name] = time.time()
     #convert sensor_msg image to cv2 image
     img = bridge.imgmsg_to_cv2(raw_img, "bgr8")
@@ -39,7 +41,7 @@ def save_images_to_video(shutdown=True):
         if len(imgs.get(camera_name)) == 0: continue
         #get names of all previously saved video files for this camera
         vid_filenames = [f for f in listdir(out) if isfile(join(out, f))]
-        this_camera_filenames = [f for f in vid_filenames if camera_name in v]
+        this_camera_filenames = [f for f in vid_filenames if camera_name in f]
         #get the largest number/id in the list of video files (we add 1 to this id to generate the new video filename)
         this_camera_filenames.append("-1") #in case there are no files, id will be set to -1 + 1 = 0
         last_used_video_id = max([int(re.search(r'\d+', f).group()) for f in this_camera_filenames])
@@ -50,12 +52,12 @@ def save_images_to_video(shutdown=True):
             with open(video_filename + ".pickle") as f:
                 pickle.dump(imgs[camera_name], f)
         else:
-            #write the video to file system as a video (slower)
+            #if not shutting down, write the video to file system as a video (slower)
             height, width, colors = imgs[camera_name][0].shape
-            out = cv2.VideoWriter(video_filename + ".mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps[camera_name], (width, height))
+            vid_out = cv2.VideoWriter(video_filename + ".mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps[camera_name], (width, height))
             for i in imgs[camera_name]:
-                out.write(i)
-            out.release()
+                vid_out.write(i)
+            vid_out.release()
         #clear image array
         imgs[camera_name] = []
 
@@ -70,7 +72,7 @@ if __name__ == '__main__':
     lastFrameTime = {} #save last time a frame was received to automatically save video if feed is interrupted for a long period
     # initially a large value to make sure first frame is not saved seperately even if startup takes a while
     videoSaveTime = 2 #if 2 seconds go by with no frame received then save previous accumulated frames seperately
-    maxFramesPerVideo = fps*60 #one minute video increments
+    maxFramesPerVideo = {cam:f*60 for cam, f in fps.items()} #two minute video increments
     bridge = CvBridge() #bridge is used to convert sensor_msg images to cv2 images
     rospy.init_node('record_cameras')
     #create subscriber callback for when new image is received on topic
