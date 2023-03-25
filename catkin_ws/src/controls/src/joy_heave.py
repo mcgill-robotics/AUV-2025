2746ecd9aa58b6146704345f109466e920d96815
Avery Straumann <avery.straumann@mail.mcgill.ca>
2023-03-25 15:10:47 -0400

    rewrote joy_heave

--- catkin_ws/src/controls/src/joy_heave.py
+++ catkin_ws/src/controls/src/joy_heave.py
@@ -5,30 +5,15 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Float64
 
 
-
-positive_pub = rospy.Publisher('heave', Float64, queue_size=50)
-negative_pub = rospy.Publisher('heave', Float64, queue_size=50)
-heave_up = False
-heave_down = False
-
+pub = rospy.Publisher('heave', Float64, queue_size=50)
 
 def heave_cb(joy_msg):
     if joy_msg.buttons[1] == 1 and joy_msg.buttons[5] == 1: #heave up
-        heave_up = True
-        heave_down = False
+        pub.publish(Float64(3.0))
     elif joy_msg.buttons[0] == 1 and joy_msg.buttons[5] == 1: #heave down
-        heave_up = False
-        heave_down = True
-    else: #neither button pressed
-        heave_up = False
-        heave_down = False
-        positive_pub.publish(Float64(0.0))
-        negative_pub.publish(Float64(0.0))
-
-    if heave_up:
-        positive_pub.publish(Float64(3.0))
-    elif heave_down:
-        negative_pub.publish(Float64(-3.0))
+        pub.publish(Float64(-3.0))
+    else: #neither button pressed; reset heave
+        pub.publish(Float64(0.0))
 
 rospy.Subscriber('joy', Joy, heave_cb )
 
