0b41bed62c2ce518d1b20a48d54d26dbb1bbb081
ben-jamming <benhepditch@gmail.com>
2023-04-01 00:34:00 +0200

    added support for stereo camera streaming

--- catkin_ws/src/vision/src/stream_cameras.py
+++ catkin_ws/src/vision/src/stream_cameras.py
@@ -15,11 +15,29 @@ if __name__ == '__main__':
     outputImgWidth = rospy.get_param('~outputImgWidth')
     outputImgHeight = rospy.get_param('~outputImgHeight')
     framerate = rospy.get_param('~framerate')
-    outputTopic = rospy.get_param('~outputTopic')
+    stereo = rospy.get_param('~stereo')
     
-    pipeline = "nvarguscamerasrc sensor-id={} ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink".format(sensor_id, inputImgWidth, inputImgHeight, framerate, outputImgWidth, outputImgHeight, framerate)
-    
-    pub = rospy.Publisher(outputTopic, Image, queue_size=1)
+    if stereo:
+        pipeline = "nvarguscamerasrc sensor-id={} ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! appsink".format(sensor_id, inputImgWidth, inputImgHeight, framerate, outputImgWidth, outputImgHeight, framerate)
+        outputTopicLeft = rospy.get_param('~outputTopicLeft')
+        outputTopicRight = rospy.get_param('~outputTopicLeft')
+        pubL = rospy.Publisher(outputTopicLeft, Image, queue_size=1)
+        pubR = rospy.Publisher(outputTopicRight, Image, queue_size=1)
+        def process_frame(f):
+            #process frame to seperate into left and right images
+            w = f.shape[1]
+            fl = f[:, :int(w/2)]
+            fl = f[:, int(w/2):]
+            return fl, fr
+        def publish(fl,fr):
+            pubL.publish(bridge.cv2_to_imgmsg(fl, "bgr8"))
+            pubR.publish(bridge.cv2_to_imgmsg(fr, "bgr8"))
+    else:
+        pipeline = "nvarguscamerasrc sensor-id={} ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink".format(sensor_id, inputImgWidth, inputImgHeight, framerate, outputImgWidth, outputImgHeight, framerate)
+        outputTopic = rospy.get_param('~outputTopic')
+        pub = rospy.Publisher(outputTopic, Image, queue_size=1)
+        def process_frame(f): return f # no processing required for downwards camera
+        def publish(f): pub.publish(bridge.cv2_to_imgmsg(f, "bgr8"))
     
     cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
     if not cap.isOpened():
@@ -30,6 +48,6 @@ if __name__ == '__main__':
         if not ret:
             print("Error reading frame from camera")
             break
-        pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+        publish(process_frame(frame))
     cap.release()
         
