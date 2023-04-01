75bde3f0f032dc93fc66066ba890ef44a84824e7
ben-jamming <benhepditch@gmail.com>
2023-04-01 20:37:04 +0200

    working

--- catkin_ws/src/vision/src/stream_cameras.py
+++ catkin_ws/src/vision/src/stream_cameras.py
@@ -26,19 +26,19 @@ if __name__ == '__main__':
         def process_frame(f):
             #process frame to seperate into left and right images
             w = f.shape[1]
-            fl = f[:, :int(w/2)]
-            fr = f[:, int(w/2):]
+            fl = f[:, :int(w/2)-2]
+            fr = f[:, int(w/2)-2:]
             return fl, fr
         def publish(f):
-            fl,fr = f
+            fl,fr = process_frame(f)
             pubL.publish(bridge.cv2_to_imgmsg(fl, "bgr8"))
             pubR.publish(bridge.cv2_to_imgmsg(fr, "bgr8"))
     else:
         pipeline = "nvarguscamerasrc sensor-id={} ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw(memory:NVMM),width={},height={},framerate={}/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink".format(sensor_id, inputImgWidth, inputImgHeight, framerate, outputImgWidth, outputImgHeight, framerate)
         outputTopic = rospy.get_param('~outputTopic')
         pub = rospy.Publisher(outputTopic, Image, queue_size=1)
-        def process_frame(f): return f # no processing required for downwards camera
-        def publish(f): pub.publish(bridge.cv2_to_imgmsg(f, "bgr8"))
+        def publish(f):
+            pub.publish(bridge.cv2_to_imgmsg(f, "bgr8"))
     
     cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
     if not cap.isOpened():
@@ -49,6 +49,6 @@ if __name__ == '__main__':
         if not ret:
             print("Error reading frame from camera")
             break
-        publish(process_frame(frame))
+        publish(frame)
     cap.release()
         
