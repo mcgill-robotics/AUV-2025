39385eb416f82dc555f49a4a42fed3c600be3b3e
anthony <anthonytan012012@gmail.com>
2023-04-05 22:53:41 -0400

    untest dvl pos publisher for dvl

--- catkin_ws/src/dvl/src/dvl.py
+++ catkin_ws/src/dvl/src/dvl.py
@@ -2,10 +2,20 @@
 
 import rospy
 import serial
+import datetime
+import math
+from std_msgs.msg import Float64
+
+def get_float(token):
+    stripped = token.strip()
+    return float(stripped)
 
 
 if __name__ == "__main__":
     rospy.init_node("teledyne_navigator")
+
+    x_pub = rospy.Publisher("state_x",Float64)
+    y_pub = rospy.Publisher("state_y",Float64)
 
     port = rospy.get_param("~port")
     baudrate = rospy.get_param("~baudrate")
@@ -24,14 +34,44 @@ if __name__ == "__main__":
     conn.flush()
 
     
-    # Set PD5 output format.
-    conn.write("PD6\n".encode())
-    # Set earth coordinate transformation.
-    conn.write("EX11111\n".encode())
-    # Start pinging.
-    conn.write("CS\n".encode())
 
+    # time = rospy.get_rostime()
+    # date = datetime.datetime.fromtimestamp(math.ceil(time.to_time()))
+    # msg = "TT{}\n".format(date.strftime("%Y/%m/%d, %H:%M:%S"))
+    # conn.write(msg.encode('ascii'))
+    # conn.flush()
+
+    # Set PD5 output format.
+
+    
+    # Set earth coordinate transformation.
+    
+    conn.write("PD6\n".encode('ascii'))
+    conn.flush()
+    conn.write("EX11111\n".encode('ascii'))
+    conn.flush()
+
+    conn.write("CS\n".encode('ascii'))
     conn.flush()
     
+    # Start pinging.
+    
+    
+
+ 
+
+
     while conn.is_open:
-        print(conn.readline().decode('ascii'))
+        #print("-------------------------")
+        line = conn.readline().decode('ascii')#print(conn.readline().decode('ascii'))
+        if(line.startswith(":BD")):
+            tokens = line.split(",")
+            north = get_float(tokens[1])
+            east = get_float(tokens[2])
+
+            x_pub.publish(north)
+            y_pub.publish(east)
+
+
+
+
