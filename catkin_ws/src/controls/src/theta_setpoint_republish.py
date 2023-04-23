a2fd735fe6c31e673fa119ab7bf56c3152350b9d
antoinedang <antoine.dangeard@gmail.com>
2023-04-23 14:27:22 -0400

    added comments to theta_setpoint_republish

--- catkin_ws/src/controls/src/theta_setpoint_republish.py
+++ catkin_ws/src/controls/src/theta_setpoint_republish.py
@@ -23,12 +23,20 @@ class StateTheta:
 
 def republishSetpoint(msg, args):
     state, pub = args
+    #get the target angle of the setpoint
     target_angle = float(msg.data)
+    #get current angle of AUV from the corresponding state function
     current_angle = state()
+    #increase/decrease the setpoint angle by 360 until it is within 180 degrees of the AUV's current angle
     while abs(target_angle-current_angle) > 180:
         if target_angle > current_angle: target_angle -= 360
         else: target_angle += 360
     pub.publish(Float64(target_angle))
+
+#NODE FUNCTION: republish incoming theta setpoints for the AUV so that they are as close to the current theta state of the AUV as possible
+#basically modulo the setpoint until it is within 180 degrees of where the AUV is currently facing
+#this way we avoid the AUV doing more than 180 degree movements to reach a setpoint, instead it will rotate via the shortest path
+#since the AUV state theta is continuous i.e. does not stop at 0/360, it keeps going into the negatives / above 360
 
 if __name__ == '__main__':
     rospy.init_node('theta_setpoint_republish')
@@ -36,6 +44,7 @@ if __name__ == '__main__':
     x_pub = rospy.Publisher('theta_x_setpoint_adjusted', Float64)
     y_pub = rospy.Publisher('theta_y_setpoint_adjusted', Float64)
     z_pub = rospy.Publisher('theta_z_setpoint_adjusted', Float64)
+    #pass the publisher and correct StateTheta callable get() function to the callback
     rospy.Subscriber('theta_x_setpoint', Float64, republishSetpoint, (x_pub, state_theta.getThetaX))
     rospy.Subscriber('theta_y_setpoint', Float64, republishSetpoint, (y_pub, state_theta.getThetaY))
     rospy.Subscriber('theta_z_setpoint', Float64, republishSetpoint, (z_pub, state_theta.getThetaZ))
