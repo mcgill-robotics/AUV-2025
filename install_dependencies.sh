5e3ec6c565041b03de2587ca5af96f9896bac358
antoinedang <antoine.dangeard@gmail.com>
2023-05-08 23:42:27 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,6 @@
+#NOTE: to avoid errors, make sure you install Ubuntu with the "Minimal installation" option selected and without third-party software enabled.
+
+
 # INSTALL ROS NOETIC
 wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
 rm ./ros_install_noetic.sh
