ff9257158686782cece4f85cc56bebdba562369a
antoinedang <antoine.dangeard@gmail.com>
2023-05-08 22:14:29 -0400

    start installation script for all AUV2023 dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,22 @@
+# INSTALL ROS NOETIC
+wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
+rm ./ros_install_noetic.sh
+
+# INSTALL CATKIN TOOLS
+sudo sh \
+    -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" \
+        > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y python3-catkin-tools
+
+# PACKAGE DEPENDENCIES
+sudo apt-get install -y ros-noetic-rosserial-arduino
+sudo apt-get install -y ros-noetic-pid
+sudo apt-get install -y ros-noetic-joy
+sudo apt-get install -y ros-noetic-joy-teleop
+sudo apt-get install -y ros-noetic-sbg-driver
+sudo apt-get install -y ros-noetic-usb-cam
+sudo apt install -y python3-pip
+pip3 install numpy-quaternion
+pip3 install ultralytics
