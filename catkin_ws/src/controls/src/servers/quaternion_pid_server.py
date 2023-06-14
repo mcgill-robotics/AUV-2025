d95eda2673dcea4dbe4ee3c6fa189c0d1aad54f7
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-06-13 20:22:45 -0400

    fixes

--- catkin_ws/src/controls/src/servers/quaternion_pid_server.py
+++ catkin_ws/src/controls/src/servers/quaternion_pid_server.py
@@ -34,10 +34,34 @@ class QuaternionServer():
     def pose_callback(self, data):
         self.position = [data.position.x, data.position.y, data.position.z]
         self.body_quaternion = np.quaternion(self.pose.orientation.w, self.pose.orientation.x, self.pose.orientation.y, self.pose.orientation.z)
-        
+    
+    def cancel(self):
+        self.set_pids(self.pose)
+    
+    def callback(self, goal):
+        if(goal.displace):
+            displaced_position,displace_quaternion = self.get_goal_after_displace(goal.pose)
 
-    def convertDestination(self):
-        self.q2 = tf.transformations.quaternion_from_euler(self.setpoint_x, self.setpoint_y, self.setpoint_z)         
+            self.set_pids(displaced_position,displace_quaternion)
+        else:
+            goal_position = []
+            goal_position.append(goal.pose.position.x)
+            goal_position.append(goal.pose.position.y)
+            goal_position.append(goal.pose.position.z)
+            goal_quaternion = np.quaternion(goal.pose.orientation.w, goal.pose.orientation.x, goal.pose.orientation.y, goal.pose.orientation.z)
+            self.set_pids(goal_position,goal_quaternion)
+        
+    def get_goal_after_displace(self, goal_pose):
+        new_goal = Pose()
+        goal_position = [goal_pose.position.x, goal_pose.position.y, goal_pose.position.z]
+        displacement_quat = np.quaternion(goal_pose.orientation.w, goal_pose.orientation.x, goal_pose.orientation.y, goal_pose.orientation.z)
+        new_goal_quat = self.body_quaternion * displacement_quat
+        return goal_position,new_goal_quat
+
+    # Felipe's code
+    def convertDestination(self,eulers):
+        theta_x , theta_y, theta_z = eulers
+        self.q2 = tf.transformations.quaternion_from_euler(theta_x, theta_y, theta_z)         
     
     def updateError(self):
         q1_inv = [-self.q1[0], -self.q1[1], -self.q1[2], self.q1[3]]
@@ -51,22 +75,6 @@ class QuaternionServer():
         derivative = self.Kd * self.derivative_error 
         control_effort = proportional + integration + derivative 
         pub_control_effort.publish(control_effort)
-    
-    def cancel(self):
-        self.set_pids(self.pose)
-    
-    def callback(self, goal):
-        if(goal.displace):
-            displaced = self.get_goal_after_displace(goal.pose)
-            self.set_pids(displaced)
-        else:
-            self.set_pids(goal.pose)
-        
-    def get_goal_after_displace(self, goal_pose):
-        new_goal = Pose()
-        self.position = [goal_pose.position.x, goal_pose.position.y, goal_pose.position.z]
-        displacement_quat = np.quaternion(goal_pose.orientation.w, goal_pose.orientation.x, goal_pose.orientation.y, goal_pose.orientation.z)
-        new_goal_quat = self.body_quaternion * displacement_quat
         
 
     def set_pids(self, goal_pose):
