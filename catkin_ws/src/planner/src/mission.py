9c619e46937b11fa98c0028393e28ffad1ead91e
Felipepotenza <potenza.fontes@gmail.com>
2023-06-19 21:39:09 -0400

    fixing back

--- catkin_ws/src/planner/src/mission.py
+++ catkin_ws/src/planner/src/mission.py
@@ -119,13 +119,3 @@ if __name__ == '__main__':
 
     control = Controller(rospy.Time(0))
     QuaternionTestMission()
-
-        QualiMission()
-
-
-        # ----- UNCOMMENT BELOW TO RUN MISSION(S) -----
-        #testRotationsMission()
-        #laneMarkerGridSearchMission()
-    except KeyboardInterrupt:
-        endMission("Mission end prompted by user. Killing.")
-        exit()
