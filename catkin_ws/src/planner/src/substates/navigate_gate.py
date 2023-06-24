012b2bcc5c8f44125040703a3e684f7d988b3bcf
bigbrainzelie <elie-dimitri.abdo@mail.mcgill.ca>
2023-06-23 21:10:39 -0400

    Added navagation of gate

--- catkin_ws/src/planner/src/substates/navigate_gate.py
+++ catkin_ws/src/planner/src/substates/navigate_gate.py
@@ -1,173 +1,35 @@
-#!/usr/bin/env python3
-
 import rospy
 import smach
 from .utility.vision import *
-import time, math
-import numpy as np
+from .utility.functions import *
+import math
 
-
-class centerGate(smach.State):
-    def __init__(self, control):
+class NavigateLaneMarker(smach.State):
+    def __init__(self, origin_class, control, state, mapping):
         super().__init__(outcomes=['success', 'failure'])
-        if control == None: raise ValueError("control argument is None")
         self.control = control
-        
+        self.mapping = mapping
+        self.state = state
+        self.origin_class = origin_class
+
     def execute(self, ud):
-        '''
-        Centers the gate until error is within tolerance.
-        Once the gate is centered and scale, it returns success.
-        If it loses sight of the gate for more than timeout, returns "failure".
-        Assumes the gate is entirely in view (i.e. AUV is far enough to see the whole gate)
-        '''
-        print("Starting centering of gate.")
-        global last_object_detection
+        print("Starting gate navigation.") 
+        auv_current_position = self.state.getPosition()
+        if self.origin_class == -1: # use auv current position as origin
+            origin_position = auv_current_position
+        else:
+            origin_position = (0,0,0)
 
-        timeout = 5
-        targetCenterX = 0.5
-        targetCenterY = 0.5
-        centering_tolerance = 0.1
-        centered = False
-        y_increment = 1
-        z_increment = 1
+        gate_object = self.mapping.getClosestObject(0, (origin_position[0], origin_position[1]))
 
-        startTime = time.time()
+        if gate_object is None:
+            print("No gate in object map! Failed.")
+            return 'failure'
+        
+        offset_distance = -3
+        offset = offset_distance * degreesToVector(gate_object[4])
+        self.control.rotateYaw(gate_object[4]) # bring to exact angle 
+        self.control.move(gate_object[1] + offset[0], gate_object[2] + offset[1], gate_object[3])
 
-        while not (centered):
-            if len(last_object_detection) > 0:
-                startTime = time.time()
-                center_x = last_object_detection[0]
-                center_y = last_object_detection[1]
-                last_object_detection = []
-
-                print("Gate in view at: x:{}, y:{}!".format(center_x, center_y))
-                
-                centering_error = math.sqrt((center_x - targetCenterX)**2 + (center_y - targetCenterY)**2)
-
-                if centering_error > centering_tolerance: #isn't centered
-                    delta_y = (targetCenterX - center_x)*y_increment
-                    delta_z = (targetCenterY - center_y)*z_increment
-                    print("Swaying {}".format(delta_y))
-                    print("Heaving {}".format(delta_z))
-                    self.control.moveDeltaLocal((0,delta_y,delta_z))
-                    centered = False
-                else:
-                    centered = True
-            else:
-                print("No detection.... Timeout in " + str((startTime + timeout) - time.time()), end='\r')
-                if time.time() > startTime + timeout:
-                    print("Gate no longer visible.")
-                    return 'failure'
-                    
-        print("Successfully centered gate!")
+        print("Successfully rotated to lane marker!")
         return 'success'
-
-class goThroughGate(smach.State):
-    def __init__(self, control, extraSurge=0, extraRotation=(0,0,0)):
-        super().__init__(outcomes=['success'])
-        self.control = control
-        self.extraSurge = extraSurge
-        self.extraRotation = extraRotation
-        
-    def execute(self, ud):
-        '''
-        Moves the AUV forward until the gate is no longer in view.
-        Once gate is no longer in view, it rotates the AUV by extraRotation.
-        Once rotated, the AUV will move forward by extraSurge.
-        '''
-        global last_object_detection
-
-        noDetectionTime = 2 #wait _ seconds before assuming gate no longer in view
-        lastGateDetectionTime = time.time()
-        surgeSpeed = 1
-
-        print("Starting movement through gate.")
-        self.control.forceLocal(surgeSpeed,0)
-        while time.time() < lastGateDetectionTime + noDetectionTime:
-            if len(last_object_detection) > 0:
-                lastGateDetectionTime = time.time()
-                last_object_detection = []
-        self.control.forceLocal(0,0)
-        print("Finished going through gate.")
-
-        print("Rotating by {}.".format(self.extraRotation))
-        self.control.rotateDelta(self.extraRotation)
-
-        print("Surging by {}.".format(self.extraSurge))
-        self.control.moveDeltaLocal((self.extraSurge, 0, 0))
-        return 'success'
-
-class goAroundPole(smach.State):
-    def __init__(self, control):
-        super().__init__(outcomes=['success'])
-        self.control = control
-        
-    def execute(self, ud):
-        '''
-        Goes around a pole by doing 3 forward movements and 3 right angle turns.
-        '''
-        rotation = (0, 0, 90)
-        surgeAmt = 1
-        for i in range(3):
-            print("Surging by {} then rotating by 90 degrees.".format(surgeAmt))
-            self.control.moveDeltaLocal((surgeAmt, 0, 0))
-            self.control.rotateDelta(rotation)
-
-        return 'success'
-
-
-#ignore unnecessary object detection information and make object detection event one array
-#returns first object detection that is valid (assumes no duplicate gates in viewframe)
-def parseMessage(msg):
-    detection = []
-    for i in range(len(msg.label)):
-        if msg.camera == 0: #ignore detection on down camera
-            continue
-        if msg.label[i] != 1: #ignore detection of objects other than gate
-            continue
-        if len(detection) > 0 and detection[8] > msg.confidence[i]: #keep highest confidence prediction
-            continue
-        detection = [ msg.bounding_box_x[i],
-            msg.bounding_box_y[i],
-            msg.bounding_box_width[i],
-            msg.bounding_box_height[i] ]
-    return detection
-
-last_object_detection = []
-firstMessage = False
-
-def objectDetectCb(msg):
-    global last_object_detection
-    global firstMessage
-    firstMessage = True
-    #in the future replace with finding gate nearby if any
-    last_object_detection = parseMessage(msg)
-
-class NavigateQualiGate(smach.State):
-    def __init__(self, control=None):
-        super().__init__(outcomes=['success'])
-        if control == None: raise ValueError("Must pass in controller as control argument.")
-        self.control = control
-        
-    def execute(self, ud):
-        self.detector = ObjectDetector(msgHandler=objectDetectCb)
-        self.detector.start()
-
-        sm = smach.StateMachine(outcomes=['success', 'failure']) 
-        with sm:
-            smach.StateMachine.add('centerGate', centerGate(control=self.control), 
-                    transitions={'success': 'goThroughGate', 'failure':'failure'})
-            smach.StateMachine.add('goThroughGate', goThroughGate(control=self.control, extraRotation=(0,0,45), extraSurge=0), 
-                    transitions={'success': 'goAroundPole'})
-            smach.StateMachine.add('goAroundPole', goAroundPole(control=self.control), 
-                    transitions={'success': 'centerGateAfterPole'})
-            smach.StateMachine.add('centerGateAfterPole', centerGate(control=self.control), 
-                    transitions={'success': 'goThroughGateFinal', 'failure':'failure'})
-            smach.StateMachine.add('goThroughGateFinal', goThroughGate(control=self.control, extraRotation=(0,0,45), extraSurge=3), 
-                    transitions={'success': 'success'})
-        while not firstMessage: pass
-        print("Received first detection message. Starting state machine.")
-        res = sm.execute()
-        self.detector.stop()
-        del self.detector
-        print("Ending state machine.")
--- catkin_ws/src/planner/src/substates/navigate_lane_marker.py
+++ catkin_ws/src/planner/src/substates/navigate_lane_marker.py
@@ -3,6 +3,7 @@
 import rospy
 import smach
 from .utility.vision import *
+from .utility.functions import *
 import math
 
 class NavigateLaneMarker(smach.State):
@@ -12,23 +13,6 @@ class NavigateLaneMarker(smach.State):
         self.mapping = mapping
         self.state = state
         self.origin_class = origin_class
-        
-    def degreesToVector(self, angleDegrees):
-        angleRadians = (angleDegrees - 90) * math.pi / 18
-        x = math.cos(angleRadians)
-        y = math.sin(angleRadians)
-        return [x, y]
-
-    def normalize_vector(self, vector2D):
-        magnitude = math.sqrt(vector2D[0] ** 2 + vector2D[1] ** 2)
-        if magnitude == 0:
-            return vector2D
-        normalized_x = vector2D[0] / magnitude
-        normalized_y = vector2D[1] / magnitude
-        return [normalized_x, normalized_y]
-
-    def dotProduct(self, v1, v2):
-        return v1[0]*v2[0] + v1[1]*v2[1]
 
     def execute(self, ud):
         print("Starting lane marker navigation.") 
@@ -52,12 +36,12 @@ class NavigateLaneMarker(smach.State):
             heading2 = lane_marker_obj[5]
 
         # find heading which is pointing the least towards the AUV
-        lane_marker_heading1_vec = self.normalizeVector(self.degreesToVector(heading1))
-        lane_marker_heading2_vec = self.normalizeVector(self.degreesToVector(heading2))
-        lane_marker_to_auv_vec = self.normalizeVector((lane_marker_obj[0] - origin_position[0], lane_marker_obj[1] - origin_position[1]))
+        lane_marker_heading1_vec = normalize_vector(degreesToVector(heading1))
+        lane_marker_heading2_vec = normalize_vector(degreesToVector(heading2))
+        lane_marker_to_auv_vec = normalize_vector((lane_marker_obj[0] - origin_position[0], lane_marker_obj[1] - origin_position[1]))
 
-        heading1_dot = self.dotProduct(lane_marker_to_auv_vec, lane_marker_heading1_vec)
-        heading2_dot = self.dotProduct(lane_marker_to_auv_vec, lane_marker_heading2_vec)
+        heading1_dot = dotProduct(lane_marker_to_auv_vec, lane_marker_heading1_vec)
+        heading2_dot = dotProduct(lane_marker_to_auv_vec, lane_marker_heading2_vec)
 
         #   rotate to that heading
         if heading1_dot < heading2_dot: self.control.rotate((0,0,heading1))
