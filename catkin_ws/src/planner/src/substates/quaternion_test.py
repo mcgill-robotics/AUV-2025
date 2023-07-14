5235c7251cbd6e79b15659f857c9bcc0e668b273
Vasily <vasilyfedotov12@gmail.com>
2023-07-13 22:02:57 -0400

    implement QuaternionTest state machine comprised of PositionState and QuaternionState states - takes AUV through several consecutive rotations - TODO - controls may need to be altered to achieve rotation in auv frame

--- catkin_ws/src/planner/src/substates/quaternion_test.py
+++ catkin_ws/src/planner/src/substates/quaternion_test.py
@@ -2,25 +2,51 @@
 
 import rospy
 import smach
+import numpy as np
+import quaternion
+
 from std_msgs.msg import Float64
 
-class QuaternionTest(smach.State):
+class QuaternionTest(smach.StateMachine):
     def __init__(self, control):
         super().__init__(outcomes=['success', 'failure'])
+
+        init_pos = [0, 0, -1]
+        q_roll_90 = np.quaternion(0.707, 0.707, 0, 0)
+        q_pitch_90 = np.quaternion(0.707, 0, 0.707, 0)
+        q_yaw_90 = np.quaternion(0.707, 0, 0, 0.707)
+        q_arbitrary = np.quaternion(0.74222962, -0.20852589, 0.55627498, 0.31011337)
+
+        self.add('initial_position', PositionState(control=control, position=init_pos), 
+                transitions={'success': 'pitch_90', 'failure':'failure'})
+        self.add('roll_90', QuaternionState(control=control, quaternion=q_roll_90), 
+                transitions={'success': 'pitch_90', 'failure':'failure'})
+        self.add('pitch_90', QuaternionState(control=control, quaternion=q_pitch_90), 
+                transitions={'success': 'yaw_90', 'failure':'failure'})
+        self.add('yaw_90', QuaternionState(control=control, quaternion=q_yaw_90), 
+                transitions={'success': 'arbitrary_pose', 'failure':'failure'})
+        self.add('arbitrary_pose', QuaternionState(control=control, quaternion=q_arbitrary), 
+                transitions={'success': 'success', 'failure':'failure'})
+    
+
+class QuaternionState(smach.State):
+    def __init__(self, control, quaternion):
+        super().__init__(outcomes=['success', 'failure'])
         self.control = control
+        self.q = quaternion
     
-    def execute(self, ud):
-        print("Starting Quaternion Mission")
-        try:
-            position = [0, 0, 0]
-            quaternion = [0.7071068, 0, 0.7071068, 0]
-            self.control.quaternion_action(position, quaternion)
-            # DONE
-            print("Completed")
-            return 'success'
+    def execute(self, _):
+        quaternion = [self.q.w, self.q.x, self.q.y, self.q.z]
+        self.control.rotate(quaternion)
+        return 'success'
 
-        except KeyboardInterrupt:
-            print("Quaternion mission interrupted by used.")
-            return 'failure'    
 
+class PositionState(smach.State):
+    def __init__(self, control, position):
+        super().__init__(outcomes=['success', 'failure'])
+        self.control = control
+        self.pos = position
     
+    def execute(self, _):
+        self.control.move(self.pos)
+        return 'success'
