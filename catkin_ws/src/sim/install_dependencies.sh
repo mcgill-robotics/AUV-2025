667a37ffb354cb4a791cce54860bf28134ab90c7
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-07-16 13:28:22 -0400

    reorganize sim file structure

---
+++ catkin_ws/src/sim/install_dependencies.sh
@@ -0,0 +1,16 @@
+#GAZEBO
+git clone git@github.com:mcgill-robotics/auv-ros-ign-bridge.git
+git clone git@github.com:mcgill-robotics/auv-sim.git
+sudo apt-get update
+sudo apt-get install -y lsb-release wget gnupg
+sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y ignition-fortress
+#ROS-IGN BRIDGE
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt install -y ros-noetic-ros-ign
+export IGNITION_VERSION=fortress
+rosdep install -r --from-paths src -i -y --rosdistro noetic
