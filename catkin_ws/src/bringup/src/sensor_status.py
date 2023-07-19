578d4a6700f3b63d3e3bcfe3ae90198654b364f3
MyJetson <antoinedang>
2023-07-19 18:50:00 -0400

    fix sensor status bug

--- catkin_ws/src/bringup/src/sensor_status.py
+++ catkin_ws/src/bringup/src/sensor_status.py
@@ -38,12 +38,12 @@ class SensorStatus:
 
     def callback_dvl(self, msg):
         self.dvl_last_update = rospy.get_time()
-        self.dvl_last_values.append([msg.x.data, msg.y.data, msg.z.data, msg.roll.data, msg.pitch.data, msg.yaw.data, msg.std.data, msg.status.data])
+        self.dvl_last_values.append([msg.x, msg.y, msg.z, msg.roll, msg.pitch, msg.yaw, msg.std, msg.status])
         self.dvl_last_values = self.dvl_last_values[1:]
 
     def callback_imu(self, msg):
         self.imu_last_update = rospy.get_time()
-        self.imu_last_values.append([msg.quaternion.w.data, msg.quaternion.x.data, msg.quaternion.y.data, msg.quaternion.z.data])
+        self.imu_last_values.append([msg.quaternion.w, msg.quaternion.x, msg.quaternion.y, msg.quaternion.z])
         self.imu_last_values = self.imu_last_values[1:]
 
     def callback_down_cam(self, msg):
@@ -65,29 +65,29 @@ class SensorStatus:
             current_time = rospy.get_time()
             
             if current_time - self.depth_last_update > self.time_until_failure_detected:
-                print("\n##### ERROR: Depth sensor topic hasn't received message in {}s #####\n".format(current_time - self.depth_last_update))
+                print("##### ERROR: Depth sensor topic hasn't received message in {}s".format(current_time - self.depth_last_update))
             elif same_values(self.depth_last_values):
-                print("\n##### WARNING: Depth sensor is sending the same value #####\n") 
+                print("##### WARNING: Depth sensor is sending the same value") 
                 
             if current_time - self.dvl_last_update > self.time_until_failure_detected:
-                print("\n##### ERROR: DVL topic hasn't received message in {}s #####\n".format(current_time - self.dvl_last_update))
+                print("##### ERROR: DVL topic hasn't received message in {}s".format(current_time - self.dvl_last_update))
             elif same_values(self.dvl_last_values):
-                print("\n##### WARNING: DVL is sending the same value #####\n")
+                print("##### WARNING: DVL is sending the same value")
                 
             if current_time - self.imu_last_update > self.time_until_failure_detected:
-                print("\n##### ERROR: IMU topic hasn't received message in {}s #####\n".format(current_time - self.imu_last_update))
+                print("##### ERROR: IMU topic hasn't received message in {}s".format(current_time - self.imu_last_update))
             elif same_values(self.imu_last_values):
-                print("\n##### WARNING: IMU is sending the same value #####\n")
+                print("##### WARNING: IMU is sending the same value")
                 
             if current_time - self.down_cam_last_update > self.time_until_failure_detected:
-                print("\n##### ERROR: Down cam topic hasn't received message in {}s #####\n".format(current_time - self.down_cam_last_update))
+                print("##### ERROR: Down cam topic hasn't received message in {}s".format(current_time - self.down_cam_last_update))
             elif same_values(self.down_cam_last_values):
-                print("\n##### WARNING: Down cam is sending the same value #####\n")
+                print("##### WARNING: Down cam is sending the same value")
             
             if current_time - self.front_cam_last_update > self.time_until_failure_detected:
-                print("\n##### ERROR: Front cam topic hasn't received message in {}s #####\n".format(current_time - self.front_cam_last_update))
+                print("##### ERROR: Front cam topic hasn't received message in {}s".format(current_time - self.front_cam_last_update))
             elif same_values(self.front_cam_last_values):
-                print("\n##### WARNING: Front cam is sending the same value #####\n")
+                print("##### WARNING: Front cam is sending the same value")
             
             
 def same_values(arr):
