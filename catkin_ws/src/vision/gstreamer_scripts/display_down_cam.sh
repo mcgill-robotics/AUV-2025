08e34537e41b759267d47de41c93ac90f85e451b
antoinedang <antoine.dangeard@gmail.com>
2023-09-04 16:59:11 -0400

    cleanup

--- catkin_ws/src/vision/gstreamer_scripts/display_down_cam.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gst-launch-1.0 nvarguscamerasrc sensor-id=0 ! 'video/x-raw(memory:NVMM),width=1920,height=1080,format=(string)NV12,framerate=30/1' ! nvvidconv ! 'video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=30/1' ! nvoverlaysink
-
