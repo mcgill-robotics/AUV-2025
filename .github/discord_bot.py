8849a780d33fcf01bfbd2adc3d5abb117177e54a
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-09-13 00:32:25 -0400

    add chat gpt to clarke

--- .github/discord_bot.py
+++ .github/discord_bot.py
@@ -5,6 +5,7 @@ import sys
 import discord
 import argparse
 import requests
+import openai
 
 parser = argparse.ArgumentParser()
 parser.add_argument("issue_number")
@@ -23,11 +24,34 @@ if len(names) > 1:
     names_string += " and " + names[-1]
 
 
-message = "Hey Clarke here. Big thanks to " + names_string \
-    + " for closing issue " + str(json["number"]) + " - " + title + ". I really appreciate your hard work!"
+# message = "Hey Clarke here. Big thanks to " + names_string \
+#     + " for closing issue " + str(json["number"]) + " - " + title + ". I really appreciate your hard work!"
 
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+openai.api_key = os.getenv("CHAT_GPT_TOKEN")
+
+def chat_with_chatgpt(prompt, model="gpt-3.5-turbo"):
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=300,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = response.choices[0].text.strip()
+    return message
+
+prompt = f"You are a discord bot for autonomous underwater vehicle(AUV) projet for McGill robotics, an undergraduate student\
+    design team. As a discord bot you pretend to be the robot, \"Clarke\". You job is to give kudos to a member who just closed an\
+    issue on github. You are given the issue number and the title of the issue. You are also given the names of the assignees.\
+    The names are {names}, the issue number is {num} and the title is {title}. Your message should be funny and end with a pun\
+    that is on topic with the issue, underwater robotics, or robotics in general, and should be a positive message."
+
+
+message = chat_with_chatgpt(prompt)
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -43,10 +67,10 @@ async def send_message():
     for guild in client.guilds:
         if guild.name == "McGill Robotics":
             for channel in guild.channels:
-                if channel.name == "auv-general":
+                if channel.name == "discord-support":
                     await channel.send(message)
                     sys.exit(0)
 
 
 
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
