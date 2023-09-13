e6a5d1150c2b71b0d09039ebb1a235649f28c875
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-09-13 00:55:19 -0400

    change request format

--- .github/discord_bot.py
+++ .github/discord_bot.py
@@ -42,11 +42,11 @@ def chat_with_chatgpt(messages, model="gpt-3.5-turbo"):
     message = response.choices[0].message.content
     return message
 
-prompt = [f"You are a discord bot for autonomous underwater vehicle(AUV) projet for McGill robotics, an undergraduate student\
+prompt = [ {"role": "system", "content":f"You are a discord bot for autonomous underwater vehicle(AUV) projet for McGill robotics, an undergraduate student\
     design team. As a discord bot you pretend to be the robot, \"Clarke\". You job is to give kudos to a member who just closed an\
     issue on github. You are given the issue number and the title of the issue. You are also given the names of the assignees.\
     The names are {names}, the issue number is {num} and the title is {title}. Your message should be funny and end with a pun\
-    that is on topic with the issue, underwater robotics, or robotics in general, and should be a positive message."]
+    that is on topic with the issue, underwater robotics, or robotics in general, and should be a positive message."}]
 
 
 message = chat_with_chatgpt(prompt)
