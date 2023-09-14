390358764ffe29e92d3f9c649f6766824d24cc88
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-09-13 22:06:00 -0400

    change prompt

--- .github/discord_bot.py
+++ .github/discord_bot.py
@@ -43,10 +43,10 @@ def chat_with_chatgpt(messages, model="gpt-3.5-turbo"):
     return message
 
 prompt = [ {"role": "system", "content":f"You are a discord bot for autonomous underwater vehicle(AUV) projet for McGill robotics, an undergraduate student\
-    design team. As a discord bot you pretend to be the robot, \"Clarke\". You job is to give kudos to a member who just closed an\
+    design team. As a discord bot you pretend to be the robot, \"Clarke\". You job is to give a short kudos to a member who just closed an\
     issue on github. You are given the issue number and the title of the issue. You are also given the names of the assignees.\
-    The names are {names}, the issue number is {num} and the title is {title}. Your message should be witty, funny, sassy, sarcastic,\
-    but not mean. The assignee should find the message funny and not be offended. Keep the message short and sweet. Maximum 4 sentences."}]
+    The names are {names}, the issue number is {num} and the title is {title}. Your message should be brief, sassy and sarcastic,\
+    but not mean. Keep the message short and sweet."}]
 
 
 message = chat_with_chatgpt(prompt)
@@ -65,7 +65,7 @@ async def send_message():
     for guild in client.guilds:
         if guild.name == "McGill Robotics":
             for channel in guild.channels:
-                if channel.name == "auv-leads":
+                if channel.name == "auv-general":
                     await channel.send(message)
                     sys.exit(0)
 
