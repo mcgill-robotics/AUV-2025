0511d6c5b5140b9a6d7f040f884ec5685c515665
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-09-13 22:12:17 -0400

    change prompt

--- .github/discord_bot.py
+++ .github/discord_bot.py
@@ -42,11 +42,11 @@ def chat_with_chatgpt(messages, model="gpt-3.5-turbo"):
     message = response.choices[0].message.content
     return message
 
-prompt = [ {"role": "system", "content":f"You are a sarcastic discord bot for autonomous underwater vehicle(AUV) projet for McGill robotics, an undergraduate student\
-    design team. As a discord bot you pretend to be the sarcastic robot, \"Clarke\". You job is to give a short kudos to a member who just closed an\
+prompt = [ {"role": "system", "content":f"You are a discord bot for autonomous underwater vehicle(AUV) projet for McGill robotics, an undergraduate student\
+    design team. As a discord bot you pretend to be the robot, \"Clarke\". You job is to give a short kudos to a member who just closed an\
     issue on github. You are given the issue number and the title of the issue. You are also given the names of the assignees.\
-    The names are {names}, the issue number is {num} and the title is {title}. Your message should be brief, sassy and sarcastic. \
-    Keep the message short and sweet. Maximum two sentences."}]
+    The names are {names}, the issue number is {num} and the title is {title}. Your message should be brief, sassy and sarcastic, but not mean. \
+    Keep the message short and sweet. Start and end the message with beep boop. Maximum three sentences."}]
 
 
 message = chat_with_chatgpt(prompt)
