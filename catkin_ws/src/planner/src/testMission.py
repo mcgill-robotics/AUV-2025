e401a9557cb5807fde5be9a851acbc2e1430eb0a
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-09-26 23:09:36 -0400

    add rostest

--- catkin_ws/src/planner/src/testMission.py
+++ catkin_ws/src/planner/src/testMission.py
@@ -11,11 +11,12 @@ import rostest
 class Test1(unittest.TestCase):
     ## test 1 == 1
     def test__simple_movement(self): # only functions with 'test_'-prefix will be run!
-        controls.move([0,0,0])
+        print(state.z)
+        controls.move([0,0,-2.0])
         controls.rotate([1,0,0,0])
-        controls.move([0,0,-2])
-        controls.move([-5,-5,0])
-        controls.rotate([0.5,0.5,0.5,0.5])
+        # controls.move([0,0,-2])
+        # controls.move([-5,-5,0])
+        # controls.rotate([0.5,0.5,0.5,0.5])
         x,y,z = state.x,state.y,state.z
         qw,qx,qy,qz = state.pose.orientation.w,state.pose.orientation.x,state.pose.orientation.y,state.pose.orientation.z
         self.assertAlmostEqual(x,-5,places=0)
@@ -27,7 +28,9 @@ class Test1(unittest.TestCase):
         self.assertAlmostEqual(qz,0.5,places=2)
 
 if __name__ == '__main__':
-    rospy.init_node("Test1")
+    rospy.init_node("Test1", log_level=rospy.DEBUG)
+    rospy.sleep(10)
+    print("hello world")
     controls = Controller(rospy.Time(0))
     state = StateTracker()
     rostest.rosrun("planner", 'Test1', Test1)
