dd2e6c0c9a8dcb11095ea51c4e958a66fe8bd370
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-09-26 22:30:42 -0400

    add rostest code

--- catkin_ws/src/planner/src/testMission.py
+++ catkin_ws/src/planner/src/testMission.py
@@ -2,11 +2,32 @@
 
 import rospy
 from substates.utility.controller import Controller
-from std_msgs.msg import Bool, Empty
+from substates.utility.state import StateTracker
+import unittest
+import rostest
 
-if __name__ == "__main__":
-    rospy.init_node("moveForward",log_level=rospy.DEBUG)
+ 
+## A sample python unit test
+class Test1(unittest.TestCase):
+    ## test 1 == 1
+    def test__simple_movement(self): # only functions with 'test_'-prefix will be run!
+        controls.move([0,0,0])
+        controls.rotate([1,0,0,0])
+        controls.move([0,0,-2])
+        controls.move([-5,-5,0])
+        controls.rotate([0.5,0.5,0.5,0.5])
+        x,y,z = state.x,state.y,state.z
+        qw,qx,qy,qz = state.pose.orientation.w,state.pose.orientation.x,state.pose.orientation.y,state.pose.orientation.z
+        self.assertAlmostEqual(x,-5,places=0)
+        self.assertAlmostEqual(y,-5,places=0)
+        self.assertAlmostEqual(z,-2,places=0)
+        self.assertAlmostEqual(qw,0.5,places=2)
+        self.assertAlmostEqual(qx,0.5,places=2)
+        self.assertAlmostEqual(qy,0.5,places=2)
+        self.assertAlmostEqual(qz,0.5,places=2)
+
+if __name__ == '__main__':
+    rospy.init_node("Test1")
     controls = Controller(rospy.Time(0))
-    controls.move((0,0,0))
-
-    print("end")
+    state = StateTracker()
+    rostest.rosrun("planner", 'Test1', Test1)
