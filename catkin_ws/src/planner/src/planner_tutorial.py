2d7c070530d5ad2dab83f810f553b5cdf42ab16f
epicgamer17 <jonathan.lamontagne.kratz@gmail.com>
2023-10-21 14:22:34 -0400

    my planner :)

---
+++ catkin_ws/src/planner/src/planner_tutorial.py
@@ -0,0 +1,15 @@
+import rospy
+from substates.utility.controller import Controller
+
+forward_distance = 15
+rospy.init_node("planner_tutorial")
+rospy.sleep(25)
+
+controller = Controller(rospy.Time(0))
+controller.move([None, None, -2])
+controller.move([forward_distance, None, None])
+controller.rotateDeltaEuler([0, 0, 90])
+controller.move([None, 2, None])
+controller.rotateDeltaEuler([0, 0, 90])
+controller.move([0, None, None])
+controller.kill()
