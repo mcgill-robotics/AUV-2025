6e55ec0de178f4aec181498f09d27be008a826cd
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-12-06 22:43:14 -0500

    merge commit

--- catkin_ws/src/planner/src/quali.py
+++ catkin_ws/src/planner/src/quali.py
@@ -3,12 +3,11 @@ import rospy
 from substates.utility.controller import Controller
 
 rospy.init_node("quali")
-rospy.sleep(25)
 controls = Controller(rospy.Time(0))
 controls.rotateEuler((0,0,None))
-controls.moveDelta((None,None,-2))
-controls.forceLocal((16,0))
-rospy.sleep(60)
-controls.forceLocal((0,0))
+controls.move((0,0,-2))
+controls.rotateDeltaEuler((90,0,0))
+controls.rotateDeltaEuler((0,90,0))
+controls.rotateDeltaEuler((-90,0,0))
 controls.moveDelta((None,None,2))
 controls.kill()
