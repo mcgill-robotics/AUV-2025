926f19225d7dfa451d17f5057d1aaae29f11152d
Rain <raina.hu@mail.mcgill.ca>
2024-02-10 21:40:43 +0900

    Changed the format to unitest

--- catkin_ws/src/planner/src/object_position_test.py
+++ catkin_ws/src/planner/src/object_position_test.py
@@ -2,74 +2,72 @@
 
 import rospy
 import smach
+
 from auv_msgs.msg import VisionObjectArray
-
-import time
-
-from substates.breadth_first_search import *
 from substates.utility.controller import *
 from substates.utility.state import *
 from substates.utility.vision import *
+from substates.breadth_first_search import *
 
-def endPlanner(msg="Shutting down mission planner."):
-    print(msg)
-    control.kill()
+import unittest
+import rostest
 
-def moveAroundTarget(self): #Make a square around the target
-        control.moveDeltaLocal([4,0,0])
-        control.rotateEuler([0,0,90])
-        control.moveDeltaLocal([4,0,0])
-        control.rotateEuler([0,0,90])
-        for i in range(2):
-            control.moveDeltaLocal([8,0,0])
-            control.rotateEuler([0,0,90])
-        control.moveDeltaLocal([4,0,0])
-        control.kill()
+### Write ros testing mission for object positions on down cam ###
+    
+class ObjectPositionTest(unittest.TestCase):
+    def test__lane_marker(self): 
+        goToTarget(lane_marker_pos)
+        lane_marker_map_pos = mapping.getClass(cls="Lane Marker")
+        x = float(lane_marker_map_pos[0][1])
+        y = float(lane_marker_map_pos[0][2])
+        #z = float(lane_marker_map_pos[0][3])
+        self.assertTrue(x-1 <= x <= x+1) #Checks if x value within 1 meter of expected value
+        self.assertTrue(y-1 <= y <= y+1) 
+        #self.assertTrue(z-1 <= z <= z+1)
+    
+    def test__lane_marker2(self): #Might need to nudge AUV if it gets stuck
+        goToTarget(lane_marker_pos2)
+        lane_marker_map_pos = mapping.getClass(cls="Lane Marker")
+        x = float(lane_marker_map_pos[1][1])
+        y = float(lane_marker_map_pos[1][2])
+        #z = float(lane_marker_map_pos[1][3])
+        self.assertTrue(x-1 <= x <= x+1) #Checks if x value within 1 meter of expected value
+        self.assertTrue(y-1 <= y <= y+1) 
+        #self.assertTrue(z-1 <= z <= z+1)
 
+    def test__octagon_table(self):
+        goToTarget(octagon_table_pos)
+        octagon_table_map_pos = mapping.getClass(cls="Octagon Table")
+        x = octagon_table_map_pos[0][1]
+        y = octagon_table_map_pos[0][2]
+        #z = float(octagon_table_pos[0][3])
+        self.assertTrue(x-1 <= x <= x+1) #Checks if x value within 1 meter of expected value
+        self.assertTrue(y-1 <= y <= y+1) 
+        #self.assertTrue(z-1 <= z <= z+1)
+
+#Helper functions
 def goToTarget(target):
-    print("Moving to target")
-    x,y,z = target
-    control.move([x,y,-2], face_destination=True)
-    print("Arrived at target")
-    #moveAroundTarget()
+        print("Moving to target")
+        x,y,z = target
+        control.move([x,y,-2], face_destination=True)
+        print("Arrived at target")
 
 def object_map_cb(msg):
-    for obj in msg.objects:
-        print(obj)
+        for obj in msg.objects:
+            print(obj)
 
 if __name__ == '__main__':
-    rospy.init_node('object_position_test',log_level=rospy.DEBUG)
-    sub = rospy.Subscriber('vision/object_map', VisionObjectArray, object_map_cb)
-    rospy.on_shutdown(endPlanner)
-
-    try:
-        mapping = ObjectMapper()
-        state = StateTracker()
-        control = Controller(rospy.Time(0))
-        sm = None
-
-        print("STARTING")
-
-        #Go to approximate position of the Octagon Table and Lane Marker
-        octagon_table_pos=[-4.706,-9.036, 0]
-        lane_marker_pos=[1.661,6.204, 0]
-        lane_marker_pos2=[9.882, 1.281, 0]
-
-        goToTarget(octagon_table_pos)
-        goToTarget(lane_marker_pos)
-        goToTarget(lane_marker_pos2)
-
-        time.sleep(5)
-
-        control.kill()
-
-        octagon_table_map_pos = mapping.getClass(cls="Octagon Table")
-        lane_marker_map_pos = mapping.getClass(cls="Lane Marker")
-
-        print("Octagon Table Position: ", octagon_table_map_pos)
-        print("Lane Marker Position(s): ", lane_marker_map_pos)
-
-    except KeyboardInterrupt:
-        if sm is not None: sm.request_preempt()
-    finally:
-        endPlanner()
+    rospy.init_node('object_position_test')
+    mapping = ObjectMapper()
+    state = StateTracker()
+    control = Controller(rospy.Time(0))
+    
+    #Approximate position of the Octagon Table and Lane Marker
+    #Could maybe BFS instead
+    octagon_table_pos=[-4.706,-9.036, 0]
+    lane_marker_pos=[1.661,6.204, 0]
+    lane_marker_pos2=[9.882, 1.281, 0]
+    
+    rostest.rosrun("planner", 'object_position_test', ObjectPositionTest) #currently in planner instead of vision
+    
+    
