450c02c15eb123dab3426e6e2a13d4d9a1964b2f
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-02-16 00:18:59 -0500

    Fix front cam vision (#529)
    * WIP
    
    * cleaning + measure angle work really reliably. Position is still off (likely due to incorrect point cloud coordinates)
    
    * fixed incorrect camera info indexing
    
    * remove unnecessary comments
    
    * increase min prediction confidence because its always getting wrong detections

--- catkin_ws/src/planner/src/quali.py
+++ catkin_ws/src/planner/src/quali.py
@@ -4,19 +4,22 @@ from substates.utility.controller import Controller
 
 rospy.init_node("quali")
 controls = Controller(rospy.Time(0))
-controls.move([0,0,-2])
-controls.rotateDelta([0,1,0,0])
-controls.rotateDeltaEuler([None,0,180])
-controls.rotateDeltaEuler([90,None,None])
 
-controls.move([0,0,-2])
-controls.rotate([0,1,0,0])
-controls.rotateEuler([0,None,180])
-controls.rotateEuler([None,90,None])
+controls.flatten()
 
-controls.move([0,0,-2])
-controls.torque([10,10,0])
-rospy.sleep(1)
-controls.rotate([1,0,0,0])
+# controls.move([0,0,-2])
+# controls.rotateDelta([0,1,0,0])
+# controls.rotateDeltaEuler([None,0,180])
+# controls.rotateDeltaEuler([90,None,None])
+
+# controls.move([0,0,-2])
+# controls.rotate([0,1,0,0])
+# controls.rotateEuler([0,None,180])
+# controls.rotateEuler([None,90,None])
+
+# controls.move([0,0,-2])
+# controls.torque([10,10,0])
+# rospy.sleep(1)
+# controls.rotate([1,0,0,0])
 
 controls.kill()
