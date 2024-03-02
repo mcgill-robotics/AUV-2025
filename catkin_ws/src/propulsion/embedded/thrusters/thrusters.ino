2efb099737e7559f450c4a03e1e40207f87435c7
John-Paul Chouery <64164677+JPGC04@users.noreply.github.com>
2024-03-02 14:20:48 -0500

    new power board code

--- catkin_ws/src/propulsion/embedded/thrusters/thrusters.ino
+++ catkin_ws/src/propulsion/embedded/thrusters/thrusters.ino
@@ -1,12 +1,8 @@
-#include <ros.h>
 #include <Servo.h>
+#include <ros.h>
 #include <auv_msgs/ThrusterMicroseconds.h>
 
-/* 
-NOTE: pins 2-9 were chosen instead of 0-7, since 
-pins 0 and 1 function differently.
-
-*/
+// defines all MCU pins
 #define SRG_P_PIN 	2
 #define SRG_S_PIN	3
 #define SWY_BW_PIN 	4
@@ -16,11 +12,24 @@ pins 0 and 1 function differently.
 #define HVE_ST_S_PIN 	8
 #define HVE_ST_P_PIN 	9
 
-// int lastCommand;
+#define MCU_KS 10
+#define WATER_DETECTED 12
 
-/* less verbose identifiers
-	Pin numbers [0-7] from ThusterCommand.msg
- */
+#define TEENSY_LED 13
+
+#define TC_1 14
+#define TC_2 15
+#define TC_3 16
+#define TC_4 17
+#define TC_5 18
+#define TC_6 19
+#define TC_7 20
+#define TC_8 21
+
+#define VBAT1_SENSE 22
+#define VBAT2_SENSE 23
+
+// defines 8 thursters from ROS for initialization in an array
 const uint8_t SRG_P 	= auv_msgs::ThrusterMicroseconds::SURGE_PORT;
 const uint8_t SRG_S 	= auv_msgs::ThrusterMicroseconds::SURGE_STAR;
 const uint8_t SWY_BW 	= auv_msgs::ThrusterMicroseconds::SWAY_BOW;
@@ -30,12 +39,21 @@ const uint8_t HVE_BW_S 	= auv_msgs::ThrusterMicroseconds::HEAVE_BOW_STAR;
 const uint8_t HVE_ST_S 	= auv_msgs::ThrusterMicroseconds::HEAVE_STERN_STAR;
 const uint8_t HVE_ST_P 	= auv_msgs::ThrusterMicroseconds::HEAVE_STERN_PORT;
 
-uint16_t microseconds[] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500}; 
-
+// creates array of 8 thrusters
 Servo thrusters[8];
-const uint16_t offCommand[] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500}; 
 
-void updateThrusters(const uint16_t microseconds[8]){
+// signals to push to thrusters
+uint16_t microseconds[] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
+const uint16_t offCommand[] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
+
+// creates array for 8 thruster current sensing
+double Tcurrents[8];
+
+// creates array for 2 battery voltage sensing
+float Bvoltages[2];
+
+// updates thrusters' pwm signals from array
+void updateThrusters(const uint16_t microseconds[8]) {
 	thrusters[SRG_P].writeMicroseconds(microseconds[SRG_P]);
 	thrusters[SRG_S].writeMicroseconds(microseconds[SRG_S]);
 	thrusters[SWY_BW].writeMicroseconds(microseconds[SWY_BW]);
@@ -47,10 +65,11 @@ void updateThrusters(const uint16_t microseconds[8]){
 }
 
 void commandCb(const auv_msgs::ThrusterMicroseconds& tc){
-	 memcpy(microseconds, tc.microseconds, 8*sizeof(uint16_t));
+	memcpy(microseconds, tc.microseconds, 8*sizeof(uint16_t));
 }
 
-void initThrusters(){
+// attaches and arms thrusters
+void initThrusters() {
 	thrusters[SRG_P].attach(SRG_P_PIN);
 	thrusters[SRG_S].attach(SRG_S_PIN);
 	thrusters[SWY_BW].attach(SWY_BW_PIN);
@@ -60,26 +79,72 @@ void initThrusters(){
 	thrusters[HVE_ST_S].attach(HVE_ST_S_PIN);
 	thrusters[HVE_ST_P].attach(HVE_ST_P_PIN);
 
-	//set initial thruster effort (OFF)
 	updateThrusters(offCommand);
 }
 
 ros::NodeHandle nh;
 ros::Subscriber<auv_msgs::ThrusterMicroseconds> sub("/propulsion/microseconds", &commandCb);
 
+void killSystem() {
+	digitalWrite(MCU_KS, HIGH);
+	delay(100);
+}
+
+void powerSystem() {
+	digitalWrite(MCU_KS, LOW);
+	delay(100);
+}
+
+void waterInterrupt() {
+	killSystem();
+	while (true) {
+		digitalWrite(TEENSY_LED, HIGH);
+		delay(500);
+		digitalWrite(TEENSY_LED, LOW);
+		delay(500);
+	}
+}
+
+void senseCurrent(double Tcurrents[]) {
+	Tcurrents[0] = ((analogRead(TC_1) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[1] = ((analogRead(TC_2) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[2] = ((analogRead(TC_3) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[3] = ((analogRead(TC_4) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[4] = ((analogRead(TC_5) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[5] = ((analogRead(TC_6) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[6] = ((analogRead(TC_7) / 1024.0) * 3.3) / (0.005 * 50);
+	Tcurrents[7] = ((analogRead(TC_8) / 1024.0) * 3.3) / (0.005 * 50);
+}
+
+void senseVoltage(float Bvoltages[]) {
+	Bvoltages[0] = analogRead(VBAT1_SENSE) * (3.3 / 1024) * 1.6625 + 12.5;
+	Bvoltages[1] = analogRead(VBAT2_SENSE) * (3.3 / 1024) * 1.6625 + 12.5;
+}
+
 void setup() {
 	initThrusters();
-	pinMode(A0, INPUT_PULLUP);
+	
+	//pinMode(WATER_DETECTED, INPUT_PULLUP);
+	//pinMode(MCU_KS, OUTPUT);
+	pinMode(TC_1, INPUT);
+	pinMode(TC_2, INPUT);
+	pinMode(TC_3, INPUT);
+	pinMode(TC_4, INPUT);
+	pinMode(TC_5, INPUT);
+	pinMode(TC_6, INPUT);
+	pinMode(TC_7, INPUT);
+	pinMode(TC_8, INPUT);
+	pinMode(VBAT1_SENSE, INPUT);
+	pinMode(VBAT2_SENSE, INPUT);
+	pinMode(TEENSY_LED, OUTPUT);
+
+	//attachInterrupt(digitalPinToInterrupt(WATER_DETECTED), waterInterrupt, RISING);
+
 	nh.subscribe(sub);
 	nh.initNode();
 }
 
 void loop() {
-	//if(digitalRead(A0) == 0){
 	updateThrusters(microseconds);
-	//}
-	//else{
-	//	updateThrusters(offCommand);
-	//}
 	nh.spinOnce(); 
 }
