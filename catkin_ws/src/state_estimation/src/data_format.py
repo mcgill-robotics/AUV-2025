01fbe8931dfe96c410ef30cf1fa6fef234041fe8
Anthony <anthony.tan2@mail.mcgill.ca>
2024-03-09 17:33:21 -0600

    fix all bugs finally I think

--- catkin_ws/src/state_estimation/src/data_format.py
+++ catkin_ws/src/state_estimation/src/data_format.py
@@ -24,25 +24,17 @@ import quaternion
 #     video = cv2.VideoWriter(output_dir + f"/{title}.avi", codec, frame_rate, size)
 
 def pose_callback(msg):
-    global gps, roll, pitch, yaw, depth, seen_pose, backwards, north_offset, east_offset
+    global gps, depth, seen_pose, backwards, north_offset, east_offset
     new_north, new_east = north_offset + msg.position.x, east_offset - msg.position.y
-    gps = backwards.transform(new_north,  new_east)
+    gps = backwards.transform(new_east,  new_north)
     depth = msg.position.z
-    q_nwu_auv = np.quaternion(msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z)
-    q_ned_auv = np.quaternion(0,1,0,0) * q_nwu_auv * np.quaternion(0,1,0,0)
-    yaw, pitch, roll = transformations.euler_from_quaternion([q_ned_auv.x, q_ned_auv.y, q_ned_auv.z, q_ned_auv.w], axes='rzyx')
-    roll *= 180 / math.pi
-    pitch *= 180 / math.pi
-    yaw *= 180 / math.pi
     seen_pose = True
 
 def image_callback(msg):
     global image, seen_image
-    if video is None: return
-    seen_image = True
-    if not seen_pose: return
     data = bridge.imgmsg_to_cv2(msg, "bgr8")
     image = data
+    seen_image = True
 
 # def xyz_to_gps(x, y, z):
 #     # This function will convert the x, y, z coordinates to GPS coordinates
@@ -52,20 +44,21 @@ def image_callback(msg):
 #     longitude = -y / 1000 / km_per_deg_long + longitude_offset
 #     return latitude, longitude
 
-def init_text_file(code):
+def init_text_file():
     global output_txt, output_dir, title
     output_txt = open(output_dir + f'/{title}.txt', 'w')
-    output_txt.write(str(code))
+    output_txt.write("EPSG:4326\n")
 
 def save_data():
-    global gps, depth, image, output_txt, roll, pitch, yaw, video
+    global gps, depth, image, output_txt, video
+    print(gps,seen_image)
     if gps is not None and seen_image:
-        date = strftime("%d/%m/%Y")
+        print("saving")
         millis = str(int(round(time.time() * 1000)))[0:3]
         the_time = strftime(f"%H:%M:%S.{millis}")
         title = the_time+".jpg"
-        output_txt.write(f"{title},{gps[0]:10.15f},{gps[1]:10.15f},{depth:10.9f},{yaw:10.9f},{pitch:10.9f},{roll:10.9f}\n")
-        cv2.imwrite(title,image)
+        output_txt.write(f"{title} {gps[0]:10.13f} {gps[1]:10.13f} {depth}\n")
+        cv2.imwrite(output_dir + "/" + title,image)
 
 
 # def get_gps_factors(depth):
@@ -78,21 +71,15 @@ def save_data():
 #     return lat_factor, long_factor
 
 def shutdown():
-    global video
-    global timer
     global output_txt
     print("shutting down")
-    cv2.destroyAllWindows()
     output_txt.close()
-    video.release()
-    timer.shutdown()
 
 if __name__ == '__main__':
     rospy.init_node('data_collection')
 
     gps = None
     depth = None
-    roll, pitch, yaw = None, None, None
     seen_pose = False
     seen_image = False
     image = None
@@ -114,10 +101,9 @@ if __name__ == '__main__':
             ),
         )
     utm_crs = CRS.from_epsg(utm_crs_list[0].code)
-    print(str(utm_crs))
     forwards = Transformer.from_crs("EPSG:4326", utm_crs, always_xy=True)
     backwards = Transformer.from_crs(utm_crs, "EPSG:4326", always_xy=True)
-    north_offset, east_offset = forwards.transform(laditude_offset, laditude_offset)
+    east_offset, north_offset = forwards.transform(laditude_offset, longitude_offset)
 
 
     pose_sub = rospy.Subscriber('/state/pose', Pose, pose_callback)
@@ -128,5 +114,7 @@ if __name__ == '__main__':
     #     rospy.spin()
     # finally:
     #     shutdown()
+    init_text_file()
     while(input("press any button to capture, x to finish") !="x"):
         save_data()
+    shutdown()
