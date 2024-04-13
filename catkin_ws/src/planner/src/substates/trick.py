2a48fea592dfd2b2b7c82f093c65e41455fae82c
Vivek Kandathil <vivekandath@gmail.com>
2024-04-13 14:00:48 -0400

    Add num rolls

--- catkin_ws/src/planner/src/substates/trick.py
+++ catkin_ws/src/planner/src/substates/trick.py
@@ -26,38 +26,40 @@ class Trick(smach.State):
             return self.execute_yaw()
         #re-stabilize
         self.control.rotateEuler((0,0,None))
-
-    def get_roll(self):
-        # Extract roll angle from quaternion representation
-        # Assuming the quaternion is in the form (w, x, y, z)
-        q = self.orientation
-        roll_rad = math.atan2(2(q.wq.x + q.yq.z), 1 - 2(q.x2 + q.y2))
-        roll_deg = math.degrees(roll_rad)
-        return roll_deg
     
     def execute_roll(self):
         # Get the target roll angle in radians as a parameter
         target_roll_angle = rospy.get_param('target_roll_angle')  # in radians
 
-        print("Starting roll trick. AUV will roll until it reaches {} radians".format(target_roll_angle))
+        # The number of rolls to do
+        num_rolls = rospy.get_param('num_rolls')
+        cur_roll = 1
+
+        print("Starting roll trick. AUV will roll {} times until it reaches {} radians".format(num_rolls, target_roll_angle))
 
         # Move downwards a bit before rolling the AUV
         self.control.move((None, None, -1))
 
         # Store the initial roll angle
-        initial_roll_angle = self.control.get_roll()
+        initial_roll_angle = self.control.orientation.y + 0.00000001
 
         # Start rolling the AUV
         self.control.torque([1, 0, 0])
 
         # Check the roll angle continuously until it reaches the target angle
         while True:
-            current_roll_angle = self.control.get_roll()
+            current_roll_angle = self.control.orientation.y
+
+            print("Current", current_roll_angle, self.control.theta_y, "Target", initial_roll_angle)
 
             # Check if the absolute difference between current and initial roll angles is greater than target angle
-            if abs(current_roll_angle - initial_roll_angle) >= target_roll_angle:
-                self.control.torque([0, 0, 0])  # Stop rolling
-                break
+            if current_roll_angle >= initial_roll_angle:
+                if (cur_roll >= num_rolls):
+                    self.control.torque([0, 0, 0])  # Stop rolling
+                    break
+                else:
+                    print("Doing the next roll.")
+                    cur_roll += 1
 
             rospy.sleep(0.1)  # Adjust the sleep duration as needed to control the frequency of checking
 
