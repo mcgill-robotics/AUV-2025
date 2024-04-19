c2a31f59e76c6e1d814bdeab64d8a4de5db1771e
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-04-18 21:25:06 -0400

    update preempt messages and mission status publishes

--- catkin_ws/src/planner/src/substates/breadth_first_search.py
+++ catkin_ws/src/planner/src/substates/breadth_first_search.py
@@ -6,10 +6,11 @@ import time
 import threading
 from std_msgs.msg import String
 
-#search for objects by moving in a growing square (i.e. each side of square grows in size after every rotation)
+
+# search for objects by moving in a growing square (i.e. each side of square grows in size after every rotation)
 class BreadthFirstSearch(smach.State):
     def __init__(self, control, mapping, target_class, min_objects):
-        super().__init__(outcomes=['success', 'failure'])
+        super().__init__(outcomes=["success", "failure"])
         self.control = control
         self.mapping = mapping
         self.detectedObject = False
@@ -17,27 +18,31 @@ class BreadthFirstSearch(smach.State):
         self.target_class = target_class
         self.min_objects = min_objects
         self.expansionAmt = rospy.get_param("bfs_expansion_size")
-        self.pub_mission_display = rospy.Publisher("/mission_display", String, queue_size=1)
+        self.pub_mission_display = rospy.Publisher(
+            "/mission_display", String, queue_size=1
+        )
 
     def doBreadthFirstSearch(self):
-        movement = [0,self.expansionAmt,0]
+        movement = [0, self.expansionAmt, 0]
         while not rospy.is_shutdown():
             if self.preempt_requested():
                 break
-            #move left
+            # move left
             print("Moving by {}.".format(movement))
             self.control.moveDeltaLocal(movement, face_destination=True)
-            if self.detectedObject: return # stop grid search when object found
-            #move left by the same amount again
+            if self.detectedObject:
+                return  # stop grid search when object found
+            # move left by the same amount again
             print("Moving by {}.".format(movement))
             self.control.moveDeltaLocal(movement, face_destination=True)
-            if self.detectedObject: return # stop grid search when object found
-            #increase distance to move by
+            if self.detectedObject:
+                return  # stop grid search when object found
+            # increase distance to move by
             movement[1] += self.expansionAmt
 
     def execute(self, ud):
         print("Starting breadth-first search.")
-        self.pub_mission_display.publish("Lane")
+        self.pub_mission_display.publish("Search")
         self.control.move((None, None, rospy.get_param("nominal_depth")))
         self.control.flatten()
 
@@ -46,18 +51,18 @@ class BreadthFirstSearch(smach.State):
         startTime = time.time()
         while startTime + self.timeout > time.time() and not rospy.is_shutdown():
             if self.preempt_requested():
-                print("BFS being preempted")
+                print("BreadthFirstSearch being preempted")
                 self.service_preempt()
-                return 'failure'
+                return "failure"
             if len(self.mapping.getClass(self.target_class)) >= self.min_objects:
                 self.detectedObject = True
                 self.searchThread.join()
                 self.control.freeze_pose()
                 print("Found object! Waiting to get more observations of object.")
                 rospy.sleep(rospy.get_param("object_observation_time"))
-                return 'success'
+                return "success"
         self.detectedObject = True
         self.searchThread.join()
         self.control.freeze_pose()
         print("Breadth-first search timed out.")
-        return 'failure'   
+        return "failure"
