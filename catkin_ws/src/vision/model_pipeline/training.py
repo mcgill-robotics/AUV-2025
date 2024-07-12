34b9692dc1b2074604e9e5d8acffa1daa2cc43f7
frompotenza <potenza.fontes@gmail.com>
2024-07-12 15:15:16 -0400

    Improve training script

--- catkin_ws/src/vision/model_pipeline/training.py
+++ catkin_ws/src/vision/model_pipeline/training.py
@@ -6,56 +6,39 @@ import shutil
 from ultralytics import YOLO
 import torch
 
+
 is_front_camera_training = False # Change it to False if training for down camera.
 ################################# OS PARAMETERS ##################################
 os_name = os.name
 # Windows = nt, [Linux, Apple] = posix. 
 os_path = "\\" if os_name == "nt" else "/"  
 ##################################################################################
-############################## ROBOFLOW  PARAMETERS ##############################
-roboflow_api_key = "MJQUATZvpcKoBxRjLuXx"
-roboflow_workspace_name = "auv2024"
-dataset_version_download = "yolov8"
-if is_front_camera_training:
-     roboflow_project_name = "front-camera-sim"
-     roboflow_project_version = 6
-else:
-     roboflow_project_name = "down-camera-sim"
-     roboflow_project_version = 1
-##################################################################################
 ############################## TRAINING  PARAMETERS ##############################
 if is_front_camera_training:
-     target_classes = ["bouy", "gate", "octagon-table"]
      model_save_filename = "best_AUV_sim_front_camera_model.pt"
 else: 
-     target_classes = ["bin", "lane-marker", "octagon-table"]
      model_save_filename = "best_AUV_sim_down_camera_model.pt"
 model_name = "yolov8n.pt"
-train_test_val_split = (0.7, 0.2, 0.1)
 epoch_increments = 200
 batch_size = -1 # Auto Mode (60% GPU Memory): Use batch=-1 to automatically 
                 # adjust batch size for approximately 60% CUDA memory 
                 # utilization.
-workers = 1
+workers = 2
 cache = False
 pretrained = True
-hsv_h = 0.001
-hsv_s = 0.1
-hsv_v = 0.1
-translate = 0.1
-scale = 0.2
-fliplr = 0.2
-mosaic = 0.5
-erasing = 0.1
-crop_fraction = 0.5
-##################################################################################
-######################### CUSTOM AUGMENTATION PARAMETERS #########################
-colorAugmentProb = 0.3
-noiseAugmentProb = 0.3
-resolutionAugmentProb = 0.3
-contrastAugmentProb = 0.3
-blurAugmentProb = 0.3
-brightnessAugmentProb = 0.3
+imgsz = [480, 640]
+hsv_h = 0.015
+hsv_s = 0.3
+hsv_v = 0.3
+translate = 0.0
+scale = 0.0
+fliplr = 0.5
+flipud = 0.5
+mosaic = 0.75
+copy_paste = 0.1
+erasing = 0.0
+crop_fraction = 0.1
+degrees = 180
 ##################################################################################
 
 if __name__ == "__main__":
@@ -69,6 +52,7 @@ if __name__ == "__main__":
                model.train(
                     data=data_yaml_file_absolute_path,
                     epochs=epoch_increments,
+                    imgsz=imgsz,
                     batch=batch_size,
                     pretrained=pretrained,
                     task="detect",
@@ -80,9 +64,12 @@ if __name__ == "__main__":
                     translate=translate,
                     scale=scale,
                     fliplr=fliplr,
+                    flipud=flipud,
                     mosaic=mosaic,
+                    copy_paste=copy_paste,
                     erasing=erasing,
-                    crop_fraction=crop_fraction
+                    crop_fraction=crop_fraction,
+                    degrees=degrees
                )
                shutil.copyfile(f"runs{os_path}detect{os_path}train{os_path}weights{os_path}best.pt", model_save_filename)
           except RuntimeError as e:
