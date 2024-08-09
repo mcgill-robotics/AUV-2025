62aa9485ae0aa3891e6164b4c1a7bd26a6c7ca88
antoinedang <antoine.dangeard@gmail.com>
2024-08-09 00:57:59 -0700

    fix mistkae

--- catkin_ws/src/sensors/src/start_zed_bridge.sh
+++ catkin_ws/src/sensors/src/start_zed_bridge.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it jetson-douglas-1 bash -c "source /opt/ros/noetic/setup.bash && source /opt/ros/foxy/setup.bash && source /set_env.bash && ros2 run ros1_bridge dynamic_bridge"
+docker exec -it jetson-bridge-1 bash -c "source /opt/ros/noetic/setup.bash && source /opt/ros/foxy/setup.bash && source /set_env.bash && ros2 run ros1_bridge dynamic_bridge"
