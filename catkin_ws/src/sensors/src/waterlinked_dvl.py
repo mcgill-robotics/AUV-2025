1e045d4189e773544b38068166c6ac1e2aa016d4
Majd Khalife <majd.skhalife@gmail.com>
2024-10-13 23:57:13 -0400

    defined hamilton product

--- catkin_ws/src/sensors/src/waterlinked_dvl.py
+++ catkin_ws/src/sensors/src/waterlinked_dvl.py
@@ -8,6 +8,17 @@ import numpy as np
 
 RAD_PER_DEG = np.pi / 180.0
 
+def hamilton_product(q1,q2):
+    w1,x1,y1,z1 = q1 #define first rotation
+    w2,x2,y2,z2 = q2 #define second rotation
+
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+
+    return [w, x, y, z]
+
 def parse_velocity_report(line):
     tokens = line.split(",")
     vx = float(tokens[1])
