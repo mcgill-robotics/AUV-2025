7e1ac209b04227193c9c1e91b83ddc04e90dbee2
OwenLeSann <owen.lesann@mail.mcgill.ca>
2024-11-22 02:45:57 +0000

    refactored sensors_launch.py

--- colcon_ws/src/sensors/launch/sensors_launch.py
+++ colcon_ws/src/sensors/launch/sensors_launch.py
@@ -4,18 +4,14 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 
-
 def generate_launch_description():
-    include_files = ['sensors_status_launch.py', 'tether_status_launch.py', 'depth_sensor_and_display_launch.py', 'dvl_launch.py', 'imu_launch.py', 'hydrophones_launch.py']
-
+    launch_files = ['sensors_status_launch.py', 'tether_status_launch.py', 'depth_sensor_and_display_launch.py', 'dvl_launch.py', 'imu_launch.py', 'hydrophones_launch.py']
+    include_files = [IncludeLaunchDescription(PathJoinSubstitution(FindPackageShare('sensors'), 'launch', launch_file)).items() for launch_file in launch_files]
+    
     return LaunchDescription([
-        DeclareLaunchArgument('vision', default_value='False')
-
-        for launch_file in include_files:
-            IncludeLaunchDescription(PathJoinSubstitution(FindPackageShare('sensors'), 'launch', launch_file)).items()
-        
+        DeclareLaunchArgument('vision', default_value='False'),
         include_IncludeLaunchDescription(PathJoinSubstitution(FindPackageShare('sensors'), 'launch', 'stream-down-cam_launch.py'),
-                                        condition=IfCondition(LaunchConfiguration('vision'))).items()
+                                        condition=IfCondition(LaunchConfiguration('vision'))).items(),
         include_IncludeLaunchDescription(PathJoinSubstitution(FindPackageShare('sensors'), 'launch', 'stream-front-cam_launch.py'),
                                         condition=IfCondition(LaunchConfiguration('vision'))).items()
-    ])
+    ].append(include_files))
