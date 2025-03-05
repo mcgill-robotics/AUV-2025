04920d826f5186bd51a9563d156cf35b8d8b773e
John-Paul Chouery <john-paul.chouery@mail.mcgill.ca>
2025-03-04 23:10:22 -0500

    negative force should be compared to -0.0001 + fix to mount directions

--- colcon_ws/src/propulsion/propulsion/thrust_mapper_utils.py
+++ colcon_ws/src/propulsion/propulsion/thrust_mapper_utils.py
@@ -5,7 +5,7 @@
 MAX_FWD_FORCE = 4.52 * 9.81
 MAX_BKWD_FORCE = -3.52 * 9.81
 
-thruster_mount_dirs = [1, -1, -1, 1, 1, 0.5, 0.5, 1]
+thruster_mount_dirs = [1, -1, -1, 1, 1, -1, -1, 1]
 
 def force_to_pwm(force):
     """
@@ -19,7 +19,7 @@ def force_to_pwm(force):
     if force > 0.0001:
         return int(positiveForceCurve(force / 9.81))
 
-    elif force < 0.0001:
+    elif force < -0.0001:
         return int(negativeForceCurve(force / 9.81))
 
     # Intersection is (0.0, 1500)
