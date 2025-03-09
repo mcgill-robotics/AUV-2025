5fbc2950a8464be1f4bba3b5e96e2954b6c545f9
Alb33rt <albert931023@gmail.com>
2025-03-09 00:56:41 -0500

    Resolved sim_bridge/ launch files convertion semantic errors

--- colcon_ws/src/sim_bridge/setup.py
+++ colcon_ws/src/sim_bridge/setup.py
@@ -11,7 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml'])
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # 'sim_launch = sim_bridge.sim_launch:main',
         ],
     },
 )
