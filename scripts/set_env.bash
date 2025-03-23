07086e1bc20208e76152cc63982df383a13ac1c3
Majd Khalife <majd.skhalife@gmail.com>
2025-03-22 20:27:05 -0400

    channged set_env + made small change in compose

--- scripts/set_env.bash
+++ scripts/set_env.bash
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-# only set environment variables if the Jetson and router IPs are reachable
-if ping -c 1 192.168.0.105 &> /dev/null
-then
+# Only set environment variables if the Jetson IP is reachable
+if ping -c 1 192.168.0.105 &> /dev/null; then
   echo "Jetson IP reachable."
-  # set ROS_IP to the IP adress of this host machine
-  IP=$(ip addr show | grep wlp | grep inet | awk '{print $2}' | awk 'BEGIN{OFS=FS="/"};NF--');
+  # Attempt to extract the IP from common interfaces (wlp or eth)
+  IP=$(ip addr show | grep -E 'inet ' | grep -E 'wlp|eth' | awk '{print $2}' | cut -d/ -f1 | head -n 1)
   if [ -n "$IP" ]; then
-      export ROS_IP=$IP;
+      export ROS_IP=$IP
   else
-      export ROS_IP=192.168.0.105;
+      export ROS_IP=192.168.0.105
   fi
 
-  # master node is run on Jetson
+  # Set the master node to the Jetson
   export ROS_MASTER_URI=http://192.168.0.105:11311
 else
     echo "Jetson IP not reachable - not setting environment variables."
 fi
-
