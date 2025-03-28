872436ae0c74f714b9cfe2373703a5a96d81cc4f
Majd Khalife <majd.skhalife@gmail.com>
2025-03-27 21:46:36 -0400

    revert

--- scripts/set_env.bash
+++ scripts/set_env.bash
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-# Only set environment variables if ubuntu.local is reachable
-if ping -c 1 ubuntu.local &> /dev/null; then
-  echo "ubuntu.local is reachable."
-  export ROS_IP="ubuntu.local"
-  export ROS_MASTER_URI="http://ubuntu.local:11311"
+# only set environment variables if the Jetson and router IPs are reachable
+if ping -c 1 192.168.0.105 &> /dev/null
+then
+  echo "Jetson IP reachable."
+  # set ROS_IP to the IP adress of this host machine
+  IP=$(ip addr show | grep wlp | grep inet | awk '{print $2}' | awk 'BEGIN{OFS=FS="/"};NF--');
+  if [ -n "$IP" ]; then
+      export ROS_IP=$IP;
+  else
+      export ROS_IP=192.168.0.105;
+  fi
+
+  # master node is run on Jetson
+  export ROS_MASTER_URI=http://192.168.0.105:11311
 else
-  echo "ubuntu.local not reachable - not setting environment variables."
+    echo "Jetson IP not reachable - not setting environment variables."
 fi
