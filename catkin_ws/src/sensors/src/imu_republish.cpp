d66c15d04158e8591d36042f33e903adb5cfcf05
majdkhalife <majd.skhalife@gmail.com>
2025-06-27 22:00:03 -0400

    fix

--- catkin_ws/src/sensors/src/imu_republish.cpp
+++ catkin_ws/src/sensors/src/imu_republish.cpp
@@ -25,7 +25,7 @@ void odom_cb(const sensor_msgs::Imu::ConstPtr &msg)
 
     new_msg.linear_acceleration_covariance[0] = acceleration_variance;
     new_msg.linear_acceleration_covariance[4] = acceleration_variance;
-    new_msg.linear_acceleration_covariance[8] = 10;
+    new_msg.linear_acceleration_covariance[8] = acceleration_variance;
 
     if (new_msg.orientation.w < 0)
     {
