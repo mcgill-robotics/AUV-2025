f0aba16fe3f06862da42b5e9d2192ea275b9ac31
majdkhalife <majd.skhalife@gmail.com>
2025-07-01 22:25:16 -0400

    wasnt detecting ZEDSDK, added env variaibles - need to test

--- Docker/slam/ros_entrypoint.sh
+++ Docker/slam/ros_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
-# setup ros environment
-source "/opt/ros/noetic/setup.bash" --
+# source ROS and workspace overlays
+source "/opt/ros/noetic/setup.bash"
+source "/AUV-2025/catkin_ws/devel/setup.bash"
 exec "$@"
