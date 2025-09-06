19fba8eed735121cc9d361e83e8e43622454a8c2
Nadim Asmar <nadim.asmar@mail.mcgill.ca>
2025-09-06 17:05:27 -0400

    resotre semis.py to comp version

--- catkin_ws/src/planner/src/semis.py
+++ catkin_ws/src/planner/src/semis.py
@@ -1,80 +1,65 @@
 #!/usr/bin/env python3
 import rospy
+from substates.utility.controller import Controller
 from substates.utility.functions import countdown
-from substates.utility.controller import *
-from substates.utility.state import *
-from substates.utility.functions import *
 
 
-TARGET_FREQUENCY = 40000
-PINGER_STEP_DISTANCE = 1
-
-rospy.init_node("semis")
 pub_mission_display = rospy.Publisher("/mission_display", String, queue_size=1)
-
 
 def update_display(status):
     pub_mission_display.publish(status)
     print(status)
 
+def main():
+    rospy.init_node("semis")
 
-countdown(30)
+    timer = 5
+    update_display(f"{timer} COUNTDOWN",)
+    rospy.loginfo(f"Starting {timer} countdown")
+    countdown(timer)
 
-update_display("INIT")
-state = StateTracker()
-controls = Controller(rospy.Time(0))
+    controls = Controller(rospy.Time(0))
+    try:
+        #moveDeltaLocal(self, delta_x, delta_y, delta_z, tolerance=0.05, timeout=30)
+        #rotate(self, x: float, y: float, z: float, timeout: float = 20, tol_degrees: float = 1.0) 
+        #positive = CCW rotation
 
+        #SUBMERGE
+        controls.moveDeltaLocal(0,0, -1)
 
-update_display("MOVE2GATE")
-controls.rotateDeltaEuler([0, 0, 0]) #TODO: change rotation based on flip coin
-angle_to_gate = state.theta_z
-controls.moveDelta([None, None, -0.75])
-controls.moveDelta([1, None, None])
+        #TASK: COIN FLIP 300
+        #Tails = 180
+        #Heads = 90
+        # controls.rotate(0,0, 180)   #TODO
 
+        #TASK: GATE 250 + SLALOM
+        controls.moveDeltaLocal(19, 0, 0, tolerance = 0.1, timeout = 60)
+        rospy.sleep(2)
+        
+        #TASK: OCTAGON
+        controls.moveDeltaLocal(0,0,1, tolerance = 0.1, timeout = 60)
+        rospy.sleep(2)
+        controls.moveDeltaLocal(0, 0, -1, tolerance = 0.1, timeout = 60)
 
-update_display("TRICKS")
-for _ in range(2 * 3):
-    controls.rotateDeltaEuler((0, 0, 120))
+        #TASK: RETURN HOME
+        controls.moveDeltaLocal(-16, 0, 0, tolerance = 0.1, timeout = 60)
 
+        rospy.sleep(2)
 
-update_display("GATE")
-controls.moveDelta([2, None, None])
+        #TASK: TRICKS YAW 800
+        i=0
+        num_rotations = 8
+        while i < num_rotations: 
+            controls.rotate(0, 0, 90)
+            i+=1
 
-update_display("PINGER")
-previous_angle = None
-while not rospy.is_shutdown():
+        rospy.sleep(3)
 
-    pinger_bearing = state.pingers_bearing.get(TARGET_FREQUENCY, None)
+    except rospy.ROSInterruptException:
+        rospy.logerr("ROS Interrupt received. Stopping movements.")
+    finally:
+        rospy.loginfo("Shutting down controls…")
+        controls.kill()
 
-    if pinger_bearing is not None:
-        current_angle = vectorToYawDegrees(pinger_bearing.x, pinger_bearing.y)
-        if previous_angle is not None:
-            angle_change = abs(current_angle - previous_angle)
-            if angle_change > 180:
-                angle_change -= 180
-            if angle_change > 90:
-                break
-
-        print(f" >>>>>>>>>>>>>>>>> going to pinger @ {TARGET_FREQUENCY} Hz")
-        controls.rotateEuler(
-            (
-                0,
-                0,
-                180 + current_angle,
-            )
-        )
-        pinger_bearing_np = np.array([pinger_bearing.x, pinger_bearing.y])
-        current_distance = np.linalg.norm(pinger_bearing_np)
-        vector_auv_pinger = (
-            -pinger_bearing_np / current_distance
-        ) * PINGER_STEP_DISTANCE
-        controls.moveDelta(vector_auv_pinger)
-        previous_angle = current_angle
-
-update_display("OCTAGON")
-controls.rotateEuler((0, 0, angle_to_gate))
-controls.moveDeltaLocal((0.25, 0, 0))
-controls.rotateDeltaEuler((0, 0, -90))
-controls.moveDeltaLocal((0.5, 0, 0))
-
-controls.kill()
+if __name__ == "__main__":
+    main()
